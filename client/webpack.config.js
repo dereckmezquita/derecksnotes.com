c18010b280c38c49937cf550379cc832c63d9c20
dereckdemezquita <dereck@demezquita.com>
2022-07-03 01:39:42 -0500

    Have to use ejs-webpack-loader to get ejs templating to be rendered.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,3 +2,27 @@
 const path = require('path');
 const fs = require('fs');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.ejs$/,
+                loader: 'ejs-webpack-loader',
+            }
+        ]
+    },
+    entry: {
+        bundle: path.resolve(__dirname, 'src/entries/entry.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        filename: "bruh.html",
+        template: path.resolve(__dirname, 'src/index.ejs')
+    })],
+}
