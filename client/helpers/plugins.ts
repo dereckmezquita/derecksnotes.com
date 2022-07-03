4d16839850bdbc00392d629f7b931478023922af
dereckmezquita <dereck@mezquita.io>
2022-07-03 18:58:34 -0500

    Helper code for generating plugins array, shortens code by not having to write path.resolve over and over.

---
+++ client/helpers/plugins.ts
@@ -0,0 +1,24 @@
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+const templateFolder = '../src/';
+
+interface PluginConfig {
+    filename: string;
+    template: string;
+    chunks?: string[];
+}
+
+export const makePlugins = (configs: PluginConfig[]): Object[] => {
+    const plugins: Object[] = [];
+
+    configs.forEach(v => {
+        const temp = v.template;
+
+        v.template = path.resolve(__dirname, `${templateFolder}${temp}`),
+        plugins.push(new HtmlWebpackPlugin(v));
+    });
+
+    return plugins;
+}
