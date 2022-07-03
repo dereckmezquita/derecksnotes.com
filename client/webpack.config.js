27baaef35c1b77a685b1962dd335082d2a6b96af
dereckmezquita <dereck@mezquita.io>
2022-07-03 12:31:26 -0500

    Basic config with one page it works.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,23 +6,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
+    entry: {
+        bundle: path.resolve(__dirname, 'src/ts/index.ts')
+    },
     module: {
         rules: [
             {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
                 test: /\.ejs$/,
-                loader: 'ejs-webpack-loader',
+                loader: 'ejs-webpack-loader'
             }
         ]
     },
-    entry: {
-        bundle: path.resolve(__dirname, 'src/entries/entry.js')
+    resolve: {
+        extensions: ['.ts', '.js']
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public'),
+        clean: true
     },
-    plugins: [new HtmlWebpackPlugin({
-        filename: "bruh.html",
-        template: path.resolve(__dirname, 'src/index.ejs')
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: path.resolve(__dirname, 'src/index.ejs')
+        })
+    ],
 }
