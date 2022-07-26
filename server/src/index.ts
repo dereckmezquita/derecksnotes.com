02d3462f9aa4fc121aa89fa6e376b47691757076
dereckmezquita <dereck@mezquita.io>
2022-07-26 16:46:02 -0500

    Serving from static directory of client/public.

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+// server static files from public directory
+app.use(express.static(path.join(__dirname, '../..', 'client/public')));
+
 const listener = app.listen(3000, '0.0.0.0', () => {
     console.log(`Server is up!`);
 });
