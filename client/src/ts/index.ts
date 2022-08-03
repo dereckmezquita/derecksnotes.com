2eb3b2f45032f39fe908d9fb570f028b9cdafd3a
dereckdemezquita <dereck@demezquita.com>
2022-08-03 16:06:50 -0500

    Word count script, and figcaptions/lazy load images.

--- client/src/ts/index.ts
+++ client/src/ts/index.ts
@@ -7,3 +7,50 @@ import "./modules/flex_nav";
 
 // ------------------------
 import "./modules/clock";
+
+// add number to each figcaption text to count the number of images
+const figcaptions: HTMLElement[] = Array.from(document.querySelectorAll("figcaption"));
+
+// loop over every figcaption
+for (let i = 0; i < figcaptions.length; i++) {
+    // get the text of the figcaption
+    const text: string = figcaptions[i].innerText;
+
+    // if the text is not empty
+    if (text !== "") {
+        // add the number to the text
+        figcaptions[i].innerText = `Figure ${i + 1}: ${text}`;
+    }
+}
+
+// 
+let lazyloadImages: NodeListOf<Element> = document.querySelectorAll("img.lazy");
+let lazyloadThrottleTimeout: undefined | ReturnType<typeof setTimeout>;
+
+function lazyload() {
+    if (lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+    }
+
+    lazyloadThrottleTimeout = setTimeout(() => {
+        let scrollTop: number = window.pageYOffset;
+        
+
+        lazyloadImages.forEach((img: any) => {
+            if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+            }
+        });
+
+        if (lazyloadImages.length == 0) {
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+        }
+    }, 20);
+}
+
+document.addEventListener("scroll", lazyload);
+window.addEventListener("resize", lazyload);
+window.addEventListener("orientationChange", lazyload);
