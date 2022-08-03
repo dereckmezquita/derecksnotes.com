e713c929f2db1daa40ccef4318c53c49cf992e6e
dereckmezquita <dereck@mezquita.io>
2022-08-03 12:56:24 -0500

    Chunk sets page's title.

---
+++ client/src/ts/modules/set_entry_title.ts
@@ -0,0 +1,6 @@
+
+const title: Element = document.querySelector("title");
+const h1: string = document.querySelector("h1").innerHTML;
+
+// set the title of the page
+title.innerHTML = `Dereck's Notes | ${h1}`;
