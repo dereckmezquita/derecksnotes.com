38da882ea762d6c6847ede36af1f6c4126e8e92e
dereckdemezquita <dereck@demezquita.com>
2022-08-23 23:30:29 -0500

    Console log the address of server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { router } from './modules/routes';
 import path from 'path';
 
+const port: number = 3000;
 const app = express();
 app.use(express.json());
 app.use(router);
@@ -10,6 +11,7 @@ app.use(router);
 // server static files from public directory
 app.use(express.static(path.join(__dirname, '../..', 'client/public')));
 
-const listener = app.listen(3000, '0.0.0.0', () => {
-    console.log(`Server is up!`);
+const listener = app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is up on port: ${port}`);
+    console.log(`Visit: http://localhost:${port}/index.html`);
 });
