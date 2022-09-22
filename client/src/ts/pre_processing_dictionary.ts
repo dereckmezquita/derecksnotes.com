356694238e9d314d76e85b35d2a79d4963e7d038
dereckmezquita <dereck@mezquita.io>
2022-09-22 16:52:08 -0500

    Script for processing dictionaries html from v1 to v2.

--- client/src/ts/pre_processing_dictionary.ts
+++ client/src/ts/pre_processing_dictionary.ts
@@ -1,33 +1,85 @@
 
 import { download } from './modules/download-function';
 
-const definitions: HTMLElement[] = Array.from(document.querySelectorAll(".definition"));
+// this file is included via webpack to dictionary pages; we're going from derecksnotes.com v1 to v2
+// we need to clean up and pre-process the dictionaries
+// we want to produce separate files per dictionary entry
 
-const IDs: string[] = definitions.map(e => {
-    return(e.querySelector("a.definition-word").id)
-});
+// ------------------------
+// Step 1: manual regex and clean up of text and tags
+// ------------------------
 
-const newIDs: string[] = definitions.map(e => {
-    let word: string = (e.querySelector("a.definition-word") as HTMLElement).innerText
-    word = word.toLowerCase()
-        .replace(/(\s)+/g, "_")
-        .replace(/\-|\//g, "_")
-        .replace(/\(|\)|\,|\.|\+|\'|\→/g, "")
-        .replace(/(_)+/g, "_");
+// ------------------------
+// Step 2: run this script and copy paste result back into into HTML file
+// ------------------------
+// const definitions: HTMLElement[] = Array.from(document.querySelectorAll(".definition"));
 
-    return(word);
-});
+// const IDs: string[] = definitions.map(e => {
+//     return(e.querySelector("a.definition-word").id)
+// });
 
-// getting the html with all the section dividers etc
-// since regex replace it's ok to do this no need for HTML structure
-let HTMLstring: string = document.querySelector("ol#dictionaryList").outerHTML;
+// const newIDs: string[] = definitions.map(e => {
+//     let word: string = (e.querySelector("a.definition-word") as HTMLElement).innerText
+//     word = word.toLowerCase()
+//         .replace(/(\s)+/g, "_")
+//         .replace(/\-|\//g, "_")
+//         .replace(/\(|\)|\,|\.|\+|\'|\→/g, "")
+//         .replace(/(_)+/g, "_");
 
-for (let i = 0; i < IDs.length; i++) {
-    const IDregex: RegExp = new RegExp(`id="${IDs[i]}"`, "g");
-    HTMLstring = HTMLstring.replace(IDregex, `id="${newIDs[i]}"`);
+//     return(word);
+// });
 
-    const hrefRegex: RegExp = new RegExp(`href="#${IDs[i]}"`, "g");
-    HTMLstring = HTMLstring.replace(hrefRegex, `href="#${newIDs[i]}"`);
+// // getting the html with all the section dividers etc
+// // since regex replace it's ok to do this no need for HTML structure
+// let HTMLstring: string = document.querySelector("ol#dictionaryList").outerHTML;
+
+// for (let i = 0; i < IDs.length; i++) {
+//     const IDregex: RegExp = new RegExp(`id="${IDs[i]}"`, "g");
+//     HTMLstring = HTMLstring.replace(IDregex, `id="${newIDs[i]}"`);
+
+//     const hrefRegex: RegExp = new RegExp(`href="#${IDs[i]}"`, "g");
+//     HTMLstring = HTMLstring.replace(hrefRegex, `href="#${newIDs[i]}"`);
+// }
+
+// download("dictionary.html", HTMLstring);
+
+// ------------------------
+// Step 3: run this code which will download each entry as a separate HTML file then we will add location and information to a database
+// ------------------------
+const defintions: HTMLElement[] = Array.from(document.querySelectorAll("li.definition"));
+
+type Definition = {
+    word: string,
+    category: string,
+    html: string
 }
 
-download("dictionary.html", HTMLstring);
+const delay = (ms: number): Promise<void> => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+const definitionsJSON: Definition[] = [];
+
+(async () => {
+    for (let i = 0; i < defintions.length; i++) {
+        const name: string = defintions[i].querySelector("a.definition-word").id;
+        const data_category: string = defintions[i].getAttribute("data-dictionary");
+        const htmlString: string = defintions[i].innerHTML.toString().trim();
+    
+        definitionsJSON[i] = {
+            word: name,
+            category: data_category,
+            html: htmlString
+        }
+    
+        console.log(htmlString);
+    
+        // run download(`${name}.html`, htmlString); every 5 seconds
+        await delay(10);
+        // download(`${name}.html`, htmlString);
+    }
+})();
+
+// console.log(definitionsJSON);
