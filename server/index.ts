093aa7207180b948effea2ad87091afaec772411
dereckmezquita <dereck@mezquita.io>
2022-09-23 09:32:19 -0500

    Needed to change path one up.

--- server/index.ts
+++ server/index.ts
@@ -6,10 +6,10 @@ import path from 'path';
 const port: number = 3000;
 const app = express();
 app.use(express.json());
-// app.use(router);
+app.use(router);
 
 // server static files from public directory
-app.use(express.static(path.join(__dirname, '../..', 'client/public')));
+app.use(express.static(path.join(__dirname, '..', 'client/public')));
 
 const listener = app.listen(port, '0.0.0.0', () => {
     console.log(`Server is up on port: ${port}`);
