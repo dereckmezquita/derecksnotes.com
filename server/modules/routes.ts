0028c07924e30bdc4faa301815e5f33efd7557f7
dereckdemezquita <dereck@demezquita.com>
2022-09-25 13:10:45 -0500

    Reorganised code so I can have routes in separate folders and join at the routes.ts file which is then imported to index.ts for the server.

--- server/modules/routes.ts
+++ server/modules/routes.ts
@@ -1,52 +1,14 @@
-
 import { Router, Response } from 'express';
-import { sendRes } from './helpers';
+import { MongoClient, ObjectId } from 'mongodb';
 
-const _ = undefined;
+import { getEntries, initGetEntries } from './routes/getEntries';
+import { getLikes, initGetLikes } from './routes/getLikes';
 
 export const router = Router();
 
-router.use((req, res, next) => {
-    console.log(`[${new Date().toLocaleString()}] ${req.method} @ ${req.url}`);
-    next();
-})
+router.use(getEntries, getLikes);
 
-router.get('/getPost', (req, res) => {
-    const postId = req.query.postId;
-
-    if (typeof postId !== 'string') return sendRes(res, false, _, "Bad postId!");
-
-    sendRes(res, true, { postId });
-});
-
-// ------------------------------
-import { MongoClient } from 'mongodb';
-import { page } from './db';
-
-const client = new MongoClient('mongodb://localhost:27017');
-
-// post request for getting entries for index page
-// allows to request entries for page blog, courses, etc and number of posts
-// will listen for request for more entries
-router.post('/getEntries', (req, res) => {
-    const { section, numEntries } = req.body;
-
-    if (typeof section !== 'string' || typeof numEntries !== 'number') {
-        return sendRes(res, false, undefined, "Bad request!");
-    }
-
-    // query mongo database for entries and send back to client
-    (async () => {
-        await client.connect();
-
-        const db = client.db('entries');
-        const collection = db.collection("metadata");
-
-        // get entries full data from db who's section matches the request
-        const { docs, nextID } = await page(collection, { }, { projection: { siteSection: section } }, numEntries);
-
-        await client.close();
-
-        sendRes(res, true, { entries: docs });
-    })();
-});
+export const initDB = (client: MongoClient) => {
+    initGetEntries(client);
+    initGetLikes(client);
+}
---
+++ server/modules/routes/getEntries.ts
@@ -0,0 +1,42 @@
+import { Router, Response } from 'express';
+import { sendRes } from '../helpers';
+import { MongoClient, ObjectId } from 'mongodb';
+import { page } from '../db';
+
+export const getEntries = Router();
+
+const _ = undefined;
+
+export const initGetEntries = (client: MongoClient) => {
+    // post request for getting entries for index page
+    // allows to request entries for page blog, courses, etc and number of posts
+    // will listen for request for more entries
+    getEntries.post('/getEntries', (req, res) => {
+        const { section, pageSize, nextToken } = req.body;
+
+        const sections = ['blog', 'courses', 'exercises'];
+
+        if (typeof section !== 'string') return sendRes(res, false, _, "Invalid type for section");
+        if (!sections.includes(section)) return sendRes(res, false, _, "Invalid value for section");
+        if (typeof pageSize !== 'number') return sendRes(res, false, _, "Invalid type for pageSize");
+        if (pageSize > 30 || pageSize < 1) return sendRes(res, false, _, "Invalid size for pageSize");
+        if (typeof nextToken !== 'string' && typeof nextToken !== 'undefined') return sendRes(res, false, _, "Invalid type for nextToken");
+
+        if (nextToken) {
+            if (!ObjectId.isValid(nextToken)) {
+                return sendRes(res, false, _, "Invalid value for nextToken");
+            }
+        }
+
+        // query mongo database for entries and send back to client
+        (async () => {
+            const db = client.db('entries');
+            const collection = db.collection("metadata");
+
+            // get entries full data from db who's section matches the request
+            const { docs, nextID } = await page(collection, { siteSection: section }, pageSize, new ObjectId(nextToken)); // hard coded num of entries returned
+
+            sendRes(res, true, { entries: docs, nextToken: nextID});
+        })();
+    });
+}
