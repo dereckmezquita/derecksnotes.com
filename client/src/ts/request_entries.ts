ebaeece7769343f9e0d276aca81d1128f3d301a8
dereckmezquita <dereck@mezquita.io>
2022-09-25 00:46:59 -0500

    Basic post request for mongo entries data.

--- client/src/ts/request_entries.ts
+++ client/src/ts/request_entries.ts
@@ -1,2 +1,41 @@
 
-console.log("Hello, world!");
+
+type Entry = {
+    title: string,
+    date: string,
+    content: string
+}
+
+type EntrisResponse = {
+    entries: Entry[];
+}
+
+// print the name of the html file to the console
+let file: string = window.location.pathname;
+// remove extension
+file = file.substring(0, file.lastIndexOf('.'));
+// remove leading slash
+file = file.substring(1);
+
+// submit a post request to getEntries express server
+const getEntries = async (section: string, numEntries: number): Promise<any> => {
+    const response = await fetch('/getEntries', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            section: section === 'index' ? 'blog' : section,
+            numEntries: numEntries
+        })
+    });
+
+    return await response.json();
+}
+
+// get entries as promise await
+(async () => {
+    const res = await getEntries(file, 10);
+
+    console.log(res.data.entries[0]);
+})();
