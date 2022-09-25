1fbd38309a66f5140d2220e05f6ae9771492d777
dereckdemezquita <dereck@demezquita.com>
2022-09-25 13:11:12 -0500

    The request entries script now gets which page it's on from a hidden element on the dom.

--- client/src/ts/request_entries.ts
+++ client/src/ts/request_entries.ts
@@ -1,24 +1,8 @@
 
-
-type Entry = {
-    title: string,
-    date: string,
-    content: string
-}
-
-type EntrisResponse = {
-    entries: Entry[];
-}
-
-// print the name of the html file to the console
-let file: string = window.location.pathname;
-// remove extension
-file = file.substring(0, file.lastIndexOf('.'));
-// remove leading slash
-file = file.substring(1);
+const siteSection: string = (document.getElementById("siteSection") as HTMLInputElement).value;
 
 // submit a post request to getEntries express server
-const getEntries = async (section: string, numEntries: number): Promise<any> => {
+const getEntries = async (section: string, pageSize: number): Promise<any> => {
     const response = await fetch('/getEntries', {
         method: 'POST',
         headers: {
@@ -26,7 +10,7 @@ const getEntries = async (section: string, numEntries: number): Promise<any> => 
         },
         body: JSON.stringify({
             section: section === 'index' ? 'blog' : section,
-            numEntries: numEntries
+            pageSize: pageSize
         })
     });
 
@@ -35,7 +19,7 @@ const getEntries = async (section: string, numEntries: number): Promise<any> => 
 
 // get entries as promise await
 (async () => {
-    const res = await getEntries(file, 10);
+    const res = await getEntries(siteSection, 10);
 
-    console.log(res.data.entries[0]);
+    console.log(res.data);
 })();
