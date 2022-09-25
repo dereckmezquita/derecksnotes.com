ff4afac6901addd896c7f2b538fb90704133b92d
dereckdemezquita <dereck@demezquita.com>
2022-09-25 13:10:17 -0500

    Page function works returns objects until no more uses nextToken.

--- server/modules/db.ts
+++ server/modules/db.ts
@@ -1,17 +1,28 @@
 
-import type { Collection, ObjectId, Filter, FindOptions, Document } from 'mongodb';
+import { Collection, ObjectId, Filter, FindOptions, Document } from 'mongodb';
+
+type PageData = { docs: Document[], nextID: ObjectId | undefined };
 
 export const page = async (
     collection: Collection,
     filter: Filter<Document> = {},
-    options: FindOptions = {},
-    size: number,
+    pageSize: number,
     nextID?: ObjectId
-): Promise<{ docs: Document[], nextID: ObjectId }> => {
-    if (nextID) filter['_id'] = {$lt: nextID};
+): Promise<PageData> => {
+    if (nextID) filter['_id'] = { $lt: nextID };
 
-    const docs = await collection.find(filter, options).sort({_id: 1}).limit(size).toArray();
-    const nextID2 = docs.at(-1)?._id as ObjectId;
+    const docs: any[] = await collection.find(filter).sort({ _id: -1 }).limit(pageSize + 1).toArray();
 
-    return { docs: docs, nextID: nextID2 };
+    nextID = undefined;
+
+    if (docs[(pageSize + 1) - 1]) {
+        nextID = docs[(pageSize + 1) - 2]['_id'];
+        docs.splice(-1, 1); // remove last one
+    }
+
+    for (const doc of docs) {
+        delete doc['_id'];
+    }
+
+    return { docs: docs, nextID: nextID };
 }
