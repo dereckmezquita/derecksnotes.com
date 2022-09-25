af56260182c36c46dde4d492d08c8943da1cdc25
dereckdemezquita <dereck@demezquita.com>
2022-09-24 23:09:30 -0500

    Working on server code, nginx will deliver static files, express will act as an api.

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,14 @@
-
 import express from 'express';
 import { router } from './modules/routes';
 import path from 'path';
 
 const port: number = 3000;
 const app = express();
+
 app.use(express.json());
 app.use(router);
 
-// server static files from public directory
-app.use(express.static(path.join(__dirname, '..', 'client/public')));
-
-const listener = app.listen(port, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is up on port: ${port}`);
     console.log(`Visit: http://localhost:${port}/index.html`);
 });
