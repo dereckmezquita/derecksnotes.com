787dcae30204abb98a386f58f4fc3268377c62d8
dereckmezquita <dereck@mezquita.io>
2022-09-29 19:54:25 -0500

    Added dictionary definitions api.

---
+++ server/modules/routes/getDefinitions.ts
@@ -0,0 +1,42 @@
+import { Router } from 'express';
+import { sendRes } from '../helpers';
+import { MongoClient, ObjectId } from 'mongodb';
+import { page } from '../db';
+
+export const getDefinitions = Router();
+
+const _ = undefined;
+
+export const initGetDefinitions = (client: MongoClient) => {
+    // a post request for getting data for dictionaries page
+    // responds to requests for different dictionaries; results are filtered
+    // will listen to request for more definitions
+    getDefinitions.post('/getDefinitions', (req, res) => {
+        const { dictionary, pageSize, nextToken } = req.body;
+
+        const dictionaries = ['biology', 'chemistry']
+
+        if (typeof dictionary !== 'string') return sendRes(res, false, _, "Invalid type for dictionary");
+        if (!dictionaries.includes(dictionary)) return sendRes(res, false, _, "Invalid value for dictionary");
+        if (typeof pageSize !== 'number') return sendRes(res, false, _, "Invalid type for pageSize");
+        if (pageSize > 30 || pageSize < 1) return sendRes(res, false, _, "Invalid size for pageSize");
+        if (typeof nextToken !== 'string' && typeof nextToken !== 'undefined') return sendRes(res, false, _, "Invalid type for nextToken");
+
+        if (nextToken) {
+            if (!ObjectId.isValid(nextToken)) {
+                return sendRes(res, false, _, "Invalid value for nextToken");
+            }
+        }
+
+        // query the mongo database for definitions and send them back to the client
+        (async () => {
+            const db = client.db('dictionaries');
+            const collection = db.collection('definitions');
+
+            // get the data which matches the request dictionary
+            const { docs, nextID } = await page(collection, { dictionary: dictionary }, pageSize, new ObjectId(nextToken));
+
+            sendRes(res, true, { definitions: docs, nextToken: nextID});
+        })();
+    });
+}
