a3cfb5c6d0f4aca7a02b988ee92d60d4d6471101
dereckdemezquita <dereck@demezquita.com>
2022-09-29 19:54:25 -0500

    Added dictionary definitions api.

--- server/modules/routes.ts
+++ server/modules/routes.ts
@@ -1,5 +1,6 @@
-import { Router, Response } from 'express';
+import { Router } from 'express';
 import { MongoClient, ObjectId } from 'mongodb';
+import { initGetDefinitions } from './routes/getDefinitions';
 
 import { getEntries, initGetEntries } from './routes/getEntries';
 import { getLikes, initGetLikes } from './routes/getLikes';
@@ -9,6 +10,7 @@ export const router = Router();
 router.use(getEntries, getLikes);
 
 export const initDB = (client: MongoClient) => {
+    initGetDefinitions(client);
     initGetEntries(client);
     initGetLikes(client);
 }
