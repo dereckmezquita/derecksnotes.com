5ec5815a4bb648d9df240e8090f5271884fd4caa
dereckmezquita <dereck@mezquita.io>
2022-10-07 22:48:07 -0500

    Testing next token.

--- test/apitest.ts
+++ test/apitest.ts
@@ -32,7 +32,7 @@ import axios from 'axios';
     const res = (await axios.post('http://127.0.0.1:3000/getDefinitions', { dictionary: 'biology', pageSize: 10 })).data;
 
     if (!res.success) return console.log(res.error);
-
+    
     console.log(res);
 })().catch(err => {
     console.error(err);
