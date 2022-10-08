912ee44da30c884854ad40363220141725c13fb0
dereckmezquita <dereck@mezquita.io>
2022-10-07 22:47:55 -0500

    Request for definitions from dictionaries now supported.

--- server/modules/routes.ts
+++ server/modules/routes.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 import { MongoClient, ObjectId } from 'mongodb';
-import { initGetDefinitions } from './routes/getDefinitions';
 
+// ------------------------
+import { getDefinitions, initGetDefinitions } from './routes/getDefinitions';
 import { getEntries, initGetEntries } from './routes/getEntries';
 import { getLikes, initGetLikes } from './routes/getLikes';
 
 export const router = Router();
 
-router.use(getEntries, getLikes);
+router.use(getEntries, getLikes, getDefinitions);
 
 export const initDB = (client: MongoClient) => {
-    initGetDefinitions(client);
     initGetEntries(client);
+    initGetDefinitions(client);
     initGetLikes(client);
 }
