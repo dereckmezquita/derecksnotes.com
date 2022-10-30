472e3b1a4da684006d660557cfe23bf328fd6a8e
dereckdemezquita <dereck@demezquita.com>
2022-10-30 08:51:06 -0500

    Reorganised scripts into promisified functions for calling in order; for processing the DOM and getting data from server.

--- client/src/ts/article_bundle.ts
+++ client/src/ts/article_bundle.ts
@@ -1,26 +1,23 @@
 
 // this code is injected to displayed articles for formatting and other
 
-// ------------------------
-// get the h1 and use it to set the title of the page; appears in tab name
-import "./modules/article_title";
+import {
+    setTitle, // get the h1 and use it to set the title of the page; appears in tab name
+    dropCap, // first letter of the first paragraph is a drop cap
+    wordCount, // calculate and print word count/read time to side bar
+    formatFigures, // formats and adds counters to pages with figures using figure/figcaption tags
+    footNotes, // footnotes: <a class="foot-note" href="some-link" target="_blank">
+    getSideEntries // requests side bar content from server
+} from './modules/formatDOM';
 
-// ------------------------
-// first letter of the first paragraph is a drop cap
-import "./modules/article_drop_cap";
+// async function call all these in order blocking code
+(async () => {
+    await setTitle();
+    await getSideEntries();
 
-// ------------------------
-// calculate and print word count/read time to side bar
-import "./modules/article_word_count";
+    await dropCap();
+    await wordCount();
+    await formatFigures();
+    await footNotes();
+})();
 
-// ------------------------
-// formats and adds counters to pages with figures using figure/figcaption tags
-import "./modules/article_figures";
-
-// ------------------------
-// footnotes: <a class="foot-note" href="some-link" target="_blank">
-import "./modules/article_foot_notes";
-
-// ------------------------
-// requests side bar content from server
-import "./modules/article_request_side_entries";
