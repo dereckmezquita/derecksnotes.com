97974a4dc2f027800bd20026e81e5878f44dae9b
dereckdemezquita <dereck@demezquita.com>
2023-01-23 15:08:06 -0500

    Working on user accounts.

--- client/src/ts/modules/user_login_prompt.ts
+++ client/src/ts/modules/user_login_prompt.ts
@@ -1,6 +1,8 @@
 const loginButton: HTMLElement = document.querySelector(".user-login-icon");
 
 loginButton.addEventListener("click", () => {
+    console.log("Login button clicked");
+
     const body: HTMLBodyElement = document.querySelector("body");
     
     // create a div container we attach to the body
