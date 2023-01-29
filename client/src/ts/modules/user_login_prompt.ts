390383f0cd6d58dcb47de912765bf98c3436f5a7
dereckmezquita <dereck@mezquita.io>
2023-01-29 03:17:44 +0000

    User login prompt first draft.

--- client/src/ts/modules/user_login_prompt.ts
+++ client/src/ts/modules/user_login_prompt.ts
@@ -1,16 +1,37 @@
 const loginButton: HTMLElement = document.querySelector(".user-login-icon");
+let loginPromptOpen: boolean = false;
 
-loginButton.addEventListener("click", () => {
-    console.log("Login button clicked");
+loginButton.addEventListener("click", (event) => {
+    if (loginPromptOpen) {
+        return;
+    }
+    event.stopPropagation();
+    console.log("Creating the login prompt.")
 
-    const body: HTMLBodyElement = document.querySelector("body");
-    
-    // create a div container we attach to the body
-    const loginPrompt: HTMLDivElement = document.createElement("div");
-    loginPrompt.setAttribute("class", "login-prompt-container");
+    // Create login prompt element
+    const loginPrompt = document.createElement("div");
+    loginPrompt.classList.add("login-prompt");
+    loginPrompt.innerHTML = `
+    <form>
+      <label for="username">Username/E-mail</label>
+      <input type="text" id="username" placeholder="Enter username or e-mail" required>
+      <label for="password">Password</label>
+      <input type="password" id="password" placeholder="Enter your password" required>
+      <button type="submit">Login</button>
+      <a href="#" class="register-link">Register</a>
+      <a href="#" class="forgot-password-link">Forgot Password?</a>
+    </form>
+  `;
 
-    
+    document.body.appendChild(loginPrompt);
 
-    // ------------------------
-    body.appendChild(loginPrompt);
+    loginPromptOpen = true;
+});
+
+document.addEventListener("click", (event) => {
+    if (!(event.target as HTMLElement).closest(".login-prompt") && loginPromptOpen) {
+        console.log("Removing the login prompt.")
+        document.querySelector(".login-prompt").remove();
+        loginPromptOpen = false;
+    }
 });
