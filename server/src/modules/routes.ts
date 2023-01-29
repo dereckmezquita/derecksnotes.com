ae1d54c0d07388d01559a94e771fb1c604afc2dd
dereckmezquita <dereck@mezquita.io>
2023-01-29 23:16:24 +0000

    Added initLogin to routes; working on user login system.

--- server/src/modules/routes.ts
+++ server/src/modules/routes.ts
@@ -5,13 +5,15 @@ import { MongoClient, ObjectId } from 'mongodb';
 import { getDefinitions, initGetDefinitions } from './routes/getDefinitions';
 import { getEntries, initGetEntries } from './routes/getEntries';
 import { getLikes, initGetLikes } from './routes/getLikes';
+import { postLogin, initLogin } from './routes/postLogin';
 
 export const router = Router();
 
-router.use(getEntries, getLikes, getDefinitions);
+router.use(getEntries, getLikes, getDefinitions, postLogin);
 
 export const initDB = (client: MongoClient) => {
     initGetEntries(client);
     initGetDefinitions(client);
     initGetLikes(client);
+    initLogin(client);
 }
