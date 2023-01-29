1b9dc1a56364a43567b43c1020bab30643011637
dereckmezquita <dereck@mezquita.io>
2023-01-29 16:52:24 +0000

    Optimised code and added comments.

--- client/src/ts/modules/user_login_prompt.ts
+++ client/src/ts/modules/user_login_prompt.ts
@@ -1,37 +1,54 @@
-const loginButton: HTMLElement = document.querySelector(".user-login-icon");
-let loginPromptOpen: boolean = false;
+// flag to check if login prompt is open or not
+let isLoginPromptOpen = false;
 
+// login prompt element to hold form
+let loginPrompt: HTMLElement;
+
+// select login button
+const loginButton = document.querySelector(".user-login-icon");
+
+// add click event listener to the login button
 loginButton.addEventListener("click", (event) => {
-    if (loginPromptOpen) {
-        return;
-    }
     event.stopPropagation();
-    console.log("Creating the login prompt.")
 
-    // Create login prompt element
-    const loginPrompt = document.createElement("div");
-    loginPrompt.classList.add("login-prompt");
-    loginPrompt.innerHTML = `
-    <form>
-      <label for="username">Username/E-mail</label>
-      <input type="text" id="username" placeholder="Enter username or e-mail" required>
-      <label for="password">Password</label>
-      <input type="password" id="password" placeholder="Enter password" required>
-      <button type="submit">Login</button>
-      <a href="#" class="register-link">Register</a>
-      <a href="#" class="forgot-password-link">Forgot Password?</a>
-    </form>
-  `;
+    // return if login prompt is already open
+    if (isLoginPromptOpen) return;
 
+    isLoginPromptOpen = true;
+
+    // check if login prompt is created or not
+    if (!loginPrompt) {
+        // create the login prompt
+        loginPrompt = document.createElement("div");
+        loginPrompt.classList.add("login-prompt");
+        loginPrompt.innerHTML = `
+      <form>
+        <label for="username">Username/E-mail</label>
+        <input type="text" id="username" placeholder="Enter username or e-mail" required>
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Enter password" required>
+        <button type="submit">Login</button>
+        <a href="#" class="register-link">Register</a>
+        <a href="#" class="forgot-password-link">Forgot Password?</a>
+      </form>
+    `;
+
+        // stop propagation of click events within the login prompt
+        loginPrompt.addEventListener("click", (event) => {
+            event.stopPropagation();
+        });
+    }
+
+    // add login prompt to the body
     document.body.appendChild(loginPrompt);
-
-    loginPromptOpen = true;
 });
 
+// add click event listener to the document
 document.addEventListener("click", (event) => {
-    if (!(event.target as HTMLElement).closest(".login-prompt") && loginPromptOpen) {
-        console.log("Removing the login prompt.")
-        document.querySelector(".login-prompt").remove();
-        loginPromptOpen = false;
+    // check if clicked outside of login prompt and prompt is open
+    if (!loginPrompt.contains(event.target as HTMLElement) && isLoginPromptOpen) {
+        // remove the login prompt
+        loginPrompt.remove();
+        isLoginPromptOpen = false;
     }
 });
