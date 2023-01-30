f14f2c5ae598100c51c22ce2cb48ace92d981f10
dereckmezquita <dereck@mezquita.io>
2023-01-30 05:24:17 +0000

    User input validation function.

---
+++ client/src/ts/user_input_params.ts
@@ -0,0 +1,11 @@
+export const VALIDATOR_PARAMS_STRONG_PASSWORD: {} = {
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 0,
+    returnScore: false,
+    pointsPerUnique: 1,
+    pointsPerRepeat: 0.5,
+    pointsForContainingLower: 10, pointsForContainingUpper: 10, pointsForContainingNumber: 10, pointsForContainingSymbol: 10
+}
