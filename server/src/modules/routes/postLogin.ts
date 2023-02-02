3f90575668f90ecb935b10fa189f50cc36947f0e
dereckmezquita <dereck@mezquita.io>
2023-02-02 20:59:47 +0000

    Login end point logic working; need to make passwords match now.

--- server/src/modules/routes/postLogin.ts
+++ server/src/modules/routes/postLogin.ts
@@ -1,30 +1,37 @@
 import { Router, Request, Response } from 'express';
-import { sendRes, isValidUserInput } from '../helpers';
+import { sendRes } from '../helpers';
 import { MongoClient } from 'mongodb';
-import bcrypt from 'bcrypt';
+
+import * as argon2 from 'argon2';
 
 export const postLogin = Router();
 
 export const initLogin = (client: MongoClient) => {
     postLogin.post('/login', async (req: Request, res: Response) => {
-        const { email, password } = req.body;
-        // const db = client.db('users');
-        // const users = db.collection('account_info');
-        // const user = await users.findOne({ email });
+        // the password from client is sent hashed and salted
+        // it is sent as a textual representation of the hash+salt
+        // sha512(password+salt); salt="derecks-notes"
+        const { username, password } = (req.body as { username: string, password: string });
 
-        console.log('email: ', email, "password: ", password);
+        // TODO: consider doing security checks here
+        const db = client.db('users');
+        const collection = db.collection('account_info');
+        const user = await collection.findOne({ "email.address": username });
 
-        // if (!user) {
-        //     sendRes(res, false, null, 'Email or password is incorrect');
-        //     return;
-        // }
+        // no match to that e-mail found
+        if (!user) return sendRes(res, false, null, 'E-mail or password is incorrect');
 
-        // const passwordMatch = await bcrypt.compare(password, user.password);
-        // if (!passwordMatch) {
-        //     sendRes(res, false, null, 'Email or password is incorrect');
-        //     return;
-        // }
+        console.log(`Saved password: ${user.password}`);
+        console.log(`Received password: ${password}`);
 
-        // sendRes(res, true, { "userData": "Some user data!" });
+        // compare the password received to the registered password
+        const passwordMatch = await argon2.verify(user.password, password, { type: argon2.argon2id, parallelism: 1 });
+
+        console.log(`passwordMatch result: ${passwordMatch}`);
+
+        // password does not match
+        if (!passwordMatch) return sendRes(res, false, null, 'E-mail or password is incorrect');
+
+        sendRes(res, true, { "userData": "Some user data!" });
     });
 }
