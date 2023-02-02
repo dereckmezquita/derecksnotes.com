10a703b50712551539ad4cfc8e5858adcf96ab25
dereckmezquita <dereck@mezquita.io>
2023-02-02 05:10:48 +0000

    Added registration routes.

--- server/src/modules/routes.ts
+++ server/src/modules/routes.ts
@@ -6,14 +6,16 @@ import { getDefinitions, initGetDefinitions } from './routes/getDefinitions';
 import { getEntries, initGetEntries } from './routes/getEntries';
 import { getLikes, initGetLikes } from './routes/getLikes';
 import { postLogin, initLogin } from './routes/postLogin';
+import { postRegister, initRegister } from './routes/postRegister';
 
 export const router = Router();
 
-router.use(getEntries, getLikes, getDefinitions, postLogin);
+router.use(getEntries, getLikes, getDefinitions, postRegister, postLogin);
 
 export const initDB = (client: MongoClient) => {
     initGetEntries(client);
     initGetDefinitions(client);
     initGetLikes(client);
+    initRegister(client);
     initLogin(client);
 }
