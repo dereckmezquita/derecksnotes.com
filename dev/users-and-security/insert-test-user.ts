78b849caf28fc686d2fd673885091f6207c0b485
dereckdemezquita <dereck@demezquita.com>
2023-02-03 05:26:13 +0000

    Example now correctly hashing password client then server side before inserting to db.

--- dev/users-and-security/insert-test-user.ts
+++ dev/users-and-security/insert-test-user.ts
@@ -3,7 +3,18 @@ import { createHash } from 'crypto';
 
 import * as argon2 from 'argon2';
 
+// all of this is done client side before sending to server
+const encoder = new TextEncoder();
+const decoder = new TextDecoder('ascii');
 const salt = "derecks-notes";
+
+const passBuff = encoder.encode("my-shit-password" + salt);
+// const hashBuff = await crypto.subtle.digest('SHA-512', passBuff);
+// createHash('sha512').update("my-shit-password" + salt).digest()
+// use node's crypto library instead
+const hashBuff = createHash('sha512').update(passBuff).digest();
+
+const hashStr: string = decoder.decode(hashBuff);
 
 const user_info = {
     firstName: "Dereck",
@@ -14,13 +25,14 @@ const user_info = {
         verificationToken: undefined
     },
     username: "dereckdemezquita",
-    password: ""+createHash("sha512").update("my-shit-password" + salt).digest(),
+    password: hashStr,
     userStatistics: {
         ip_addresses: [],
         last_login: new Date()
     }
 }
 
+// all of this is done server side on registration
 const url = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(url);
 
@@ -35,8 +47,8 @@ async function insert(): Promise<void> {
     // hash the hash with argon2 before insertion
     user_info.password = await argon2.hash(user_info.password, { type: argon2.argon2id, parallelism: 1 });
 
-    // const insertResult = await collection.insertOne(user_info);
-    // console.log(`Inserted ${insertResult} documents successfully! :3`);
+    const insertResult = await collection.insertOne(user_info);
+    console.log(`Inserted ${insertResult} documents successfully! :3`);
 
     // find the user as a test
     const user = await collection.findOne({ "email.address": user_info.email.address });
