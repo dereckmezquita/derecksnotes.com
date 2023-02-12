396ec8481a1ef813a8b57936b85e6031f22dae73
dereckmezquita <dereck@mezquita.io>
2023-02-12 07:16:13 +0000

    Validate input and udpate ip/user info.

--- server/src/modules/routes/postLogin.ts
+++ server/src/modules/routes/postLogin.ts
@@ -2,7 +2,10 @@ import { Router, Request, Response } from 'express';
 import { sendRes } from '../helpers';
 import { MongoClient } from 'mongodb';
 
+import { checkEmail } from '../validators';
+
 import * as argon2 from 'argon2';
+import { logger } from '../../logger';
 
 export const postLogin = Router();
 
@@ -11,12 +14,17 @@ export const initLogin = (client: MongoClient) => {
         // the password from client is sent hashed and salted
         // it is sent as a textual representation of the hash+salt
         // sha512(password+salt); salt="derecks-notes"
-        const { username, password } = (req.body as { username: string, password: string });
+        const { email, password } = (req.body as { email: string, password: string });
+
+        // check username sent by client
+        const emailCheck = checkEmail(email);
+
+        if (!emailCheck.success) return sendRes(res, false, null, emailCheck.error);
 
         // TODO: consider doing security checks here
         const db = client.db('users');
-        const collection = db.collection('account_info');
-        const user = await collection.findOne({ "email.address": username });
+        const accounts = db.collection('accounts');
+        const user = await accounts.findOne({ "email.address": email }) as UserInfo | null;
 
         // no match to that e-mail found
         if (!user) return sendRes(res, false, null, 'E-mail or password is incorrect');
@@ -29,6 +37,33 @@ export const initLogin = (client: MongoClient) => {
         // password does not match
         if (!passwordMatch) return sendRes(res, false, null, 'E-mail or password is incorrect');
 
-        sendRes(res, true, { "userData": "Some user data!" });
+        // get the users IP address
+        const ip_address = req.headers['x-forwarded-for'] as string;
+
+        // if password matches update the user's last connected date
+        // check if the user has connected from this IP address before
+        const ip_address_index = user.userStatistics.ip_addresses.findIndex((ip_address_obj) => ip_address_obj.ip_address === ip_address);
+
+        // if the user has connected from this IP address before
+        if (ip_address_index !== -1) {
+            // udpate the user's last connected date only for that IP address
+            logger.info(`User ${user.email.address} has connected from an existing IP address: ${ip_address}.`);
+            await accounts.updateOne(
+                { "email.address": email },
+                {
+                    $set: { [`userStatistics.ip_addresses.${ip_address_index}.last_use`]: new Date() }
+                }
+            );
+        } else {
+            logger.info(`User ${user.email.address} has connected from a new IP address: ${ip_address}.`);
+            user.userStatistics.ip_addresses.push({
+                ip_address: ip_address,
+                first_use: new Date(),
+                last_use: new Date()
+            });
+        }
+
+        logger.info(`User ${user.email.address} has logged in from IP address ${ip_address}.`);
+        sendRes(res, true, "Successfully logged in!");
     });
 }
