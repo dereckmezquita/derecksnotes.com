9f8c9eae42283a627bf65ae9c3087a67f71a945f
dereckdemezquita <dereck@demezquita.com>
2023-02-12 07:15:53 +0000

    Register new user.

--- server/src/modules/routes/postRegister.ts
+++ server/src/modules/routes/postRegister.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from 'express';
 import { sendRes } from '../helpers';
 import { MongoClient } from 'mongodb';
 
+import { checkRegisterInfo } from '../validators';
+
 import * as argon2 from 'argon2';
 
 export const postRegister = Router();
@@ -9,33 +11,52 @@ export const postRegister = Router();
 export const initRegister = (client: MongoClient) => {
     postRegister.post('/register', async (req: Request, res: Response) => {
         // get account info from user
-        const user_info = req.body as UserInfo;
+        const { firstName, lastName, username, email, password } = req.body;
 
-        const e_mail: string = user_info.email.address
+        // check the inputs provided from the client; they might try to hack the client to send requests
+        const infoCheck = checkRegisterInfo(firstName, lastName, username, email);
 
+        if (!infoCheck.success) return sendRes(res, false, null, infoCheck.error);
+
+        // console.log(req.body);
         const db = client.db('users');
-        const accounts = db.collection('account_info');
-        const user = await accounts.findOne({ e_mail });
+        const accounts = db.collection('accounts');
+        const user = await accounts.findOne({ "email.address": email }) as UserInfo | null;
 
         // if a user with that e-mail already exists
-        if (user) return sendRes(res, false, null, 'Username or e-mail already in use');
+        if (user) return sendRes(res, false, null, 'Username or e-mail already in use.');
 
         // the user sends the password already salted and hashed with sha512
         // we will hash the hash with argon2 before insertion
-        user_info.password = await argon2.hash(user_info.password, { type: argon2.argon2id, parallelism: 1 });
+        const hash = await argon2.hash(password, { type: argon2.argon2id, parallelism: 1 });
 
         // get the users IP address
-        const ip_address: string = req.headers['x-forwarded-for'] as string;
+        const ip_address = req.headers['x-forwarded-for'] as string;
 
-        user_info.userStatistics.ip_addresses.push(ip_address);
+        // insert the user into the database
+        const user_info: UserInfo = {
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            password: hash,
+            email: {
+                address: email,
+                verified: false
+            },
+            userStatistics: {
+                ip_addresses: [
+                    {
+                        ip_address: ip_address,
+                        first_use: new Date(),
+                        last_use: new Date()
+                    }
+                ],
+                last_connected: new Date()
+            }
+        };
 
-        // update last login time
-        user_info.userStatistics.last_login = new Date();
-
-        // add the users IP address to the user_info object
-        user_info.userStatistics.ip_addresses.push(ip_address as string);
-
-        // insert the new user into the database
         await accounts.insertOne(user_info);
+
+        sendRes(res, true, "You're now registered!");
     });
 }
