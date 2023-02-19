3441a4b683f2834f0ebfa14140216618223b0ffb
dereckmezquita <dereck@mezquita.io>
2023-02-19 17:17:00 +0000

    Deleted some comments.

--- server/src/routes/articles/getLikes.ts
+++ server/src/routes/articles/getLikes.ts
@@ -1,14 +1,11 @@
 import { Router, Response } from 'express';
 import { sendRes } from '../../modules/helpers';
-import { MongoClient, ObjectId } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 export const getLikes = Router();
 
 export const initGetLikes = (client: MongoClient) => {
-    // post request for getting entries for index page
-    // allows to request entries for page blog, courses, etc and number of posts
-    // will listen for request for more entries
-    getLikes.get('/getLikes', (req, res) => {
+    getLikes.get('/articles/get_likes', (req, res) => {
         sendRes(res, true, {someKey: "Some value!"})
     });
 }
