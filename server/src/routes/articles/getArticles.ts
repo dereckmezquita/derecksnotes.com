3518fc3687ae544b6f605abe8aa2fe4dfae0b3b8
dereckdemezquita <dereck@demezquita.com>
2023-02-19 18:29:43 +0000

    Moved async to top level.

--- server/src/routes/articles/getArticles.ts
+++ server/src/routes/articles/getArticles.ts
@@ -6,7 +6,7 @@ import { page } from '../../modules/db';
 export const getArticles = Router();
 
 export const initGetArticles = (client: MongoClient) => {
-    getArticles.post('/articles/get_metadata', (req, res) => {
+    getArticles.post('/articles/get_metadata', async (req, res) => {
         const { section, pageSize, nextToken } = req.body;
 
         if (typeof section !== 'string') return sendRes(res, false, undefined, "Invalid type for section");
@@ -21,17 +21,15 @@ export const initGetArticles = (client: MongoClient) => {
         }
 
         // query mongo database for articles and send back to client
-        (async () => {
-            const db = client.db('articles');
-            const collection = db.collection("metadata");
+        const db = client.db('articles');
+        const collection = db.collection("metadata");
 
-            // return all articles which match siteSection and published is true
-            const { docs, nextID } = await page(collection, {
-                siteSection: section,
-                published: true
-            }, pageSize, new ObjectId(nextToken));
+        // return all articles which match siteSection and published is true
+        const { docs, nextID } = await page(collection, {
+            siteSection: section,
+            published: true
+        }, pageSize, new ObjectId(nextToken));
 
-            sendRes(res, true, { articles: docs, nextToken: nextID });
-        })();
+        sendRes(res, true, { articles: docs, nextToken: nextID });
     });
 }
