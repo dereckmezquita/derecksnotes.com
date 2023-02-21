ce98c238c4495221e7d91072e95c346e7b91ee8a
dereckmezquita <dereck@mezquita.io>
2023-02-21 05:54:38 +0000

    Refactored for cleaner code, escape/allow some html tags.

--- server/src/routes/articles/newComment.ts
+++ server/src/routes/articles/newComment.ts
@@ -1,20 +1,23 @@
 import { Router, Request, Response } from 'express';
-import { sendRes } from '../../modules/helpers';
+import { generateHashID, sendRes } from '../../modules/helpers';
 import { MongoClient } from 'mongodb';
 import { userCommentCheck } from '../../modules/validators';
-import crypto from 'crypto';
+
 import sanitizeHtml from 'sanitize-html';
 
 export const postComment = Router();
 
 export const initComment = (client: MongoClient) => {
     postComment.post('/articles/new_comment', async (req: Request, res: Response) => {
+        // ------------------------------------
+        const sessionData = req.session as SessionDataRes;
+
         // check if the user is logged in and has an active session
-        if (!((req.session as SessionDataRes).authenticated)) {
+        if (!(sessionData.authenticated)) {
             return sendRes(res, false, null, 'You must be logged in to comment.');
         }
 
-        const cookie = (req.session as SessionDataRes).user as UserCookie;
+        const cookie = sessionData.user as UserCookie;
 
         // check if the user object exists in the session
         if (!cookie) return sendRes(res, false, null, 'User not found in session; please login.');
@@ -25,13 +28,23 @@ export const initComment = (client: MongoClient) => {
         const profilePhoto: string | undefined = cookie.profilePhoto;
 
         // ------------------------------------
-        const { comment, datetime, article, replyToId } = req.body;
+        const { comment, datetime, article, replyToId } = (req.body as { comment: string, datetime: string, article: string, replyToId?: string });
+        
+        // ------------------------------------
+        // datetime provided not older than 30 seconds
+        const now: Date = new Date();
 
-        console.log(`comment: ${comment}; datetime: ${datetime}; article: ${article}; replyToId: ${replyToId}`);
+        // received datetime format: "2023-02-19T05:44:46.861Z"
+        const receivedDatetime: Date = new Date(datetime);
 
+        const timediff = Math.floor(Math.abs(now.getTime() - receivedDatetime.getTime()) / 1000);
+
+        if (timediff > 30) return sendRes(res, false, null, 'The datetime provided is not valid.');
+
+        // ------------------------------------
         // check that the comment is not empty and alphanumeric
-        const check: { success: boolean, error?: string } = userCommentCheck(comment);
-        if (!check.success) return sendRes(res, false, null, check.error);
+        const commentCheck: { success: boolean, error?: string } = userCommentCheck(comment);
+        if (!commentCheck.success) return sendRes(res, false, null, commentCheck.error);
 
         const allowedTags: string[] = [
             "h1", "h2", "h3", "span", "strong", "p", "em", "a",
@@ -46,21 +59,11 @@ export const initComment = (client: MongoClient) => {
             allowedAttributes: {
                 'a': ['href']
             },
-            allowedIframeHostnames: ['www.youtube.com']
+            allowedIframeHostnames: ['www.youtube.com'],
+            disallowedTagsMode: 'recursiveEscape'
         });
 
-        processedComment = processedComment.trim().replace(/[\r\n]+/g, '\n'); //.replace(/[\r\n]{3,}/g, '\n\n');
-
-        // check that the datetime provided is not greater than or less than 30 seconds from the current time
-        const now: Date = new Date();
-
-        // received datetime is in: "2023-02-19T05:44:46.861Z"
-        // convert to Date object
-        const receivedDatetime: Date = new Date(datetime);
-
-        const timediff = Math.floor(Math.abs(now.getTime() - receivedDatetime.getTime()) / 1000);
-
-        if (timediff > 30) return sendRes(res, false, null, 'The datetime provided is not valid.');
+        // processedComment = processedComment.trim().replace(/[\r\n]+/g, '\n'); //.replace(/[\r\n]{3,}/g, '\n\n');
 
         // ------------------------------------
         const db = client.db('articles');
@@ -70,67 +73,48 @@ export const initComment = (client: MongoClient) => {
         const ip_address = req.headers['x-forwarded-for'] as string;
 
         // create comment id from article + username + datetime then hash it
-        const comment_id: string = crypto.createHash('sha256')
-            .update(`${article}_${username}_${receivedDatetime.getTime()}`)
-            .digest('hex');
+        const comment_id: string = generateHashID(`${article}_${username}_${receivedDatetime.getTime()}`)
+
+        // ------------------------------------
+        // create comment object; will be modified and inserted
+        const newComment: UserComment = {
+            comment_id: comment_id,
+            replies_to_this: [],
+            replies_to_that: undefined,
+            article: article,
+            comment: processedComment,
+            commentInfo: {
+                datetime: receivedDatetime,
+                likes: 0,
+                dislikes: 0
+            },
+            userInfo: {
+                email: email,
+                username: username,
+                profilePhoto: profilePhoto,
+                ip_address: ip_address
+            }
+        };
 
         // insert the comment into the database
         let insertResult;
         if (replyToId) {
-            const replyComment: UserComment = {
-                comment_id: comment_id,
-                replies_to_this: [],
-                replies_to_that: replyToId,
-                article: article,
-                comment: processedComment,
-                commentInfo: {
-                    datetime: receivedDatetime,
-                    likes: 0,
-                    dislikes: 0
-                },
-                userInfo: {
-                    email: email,
-                    username: username,
-                    profilePhoto: profilePhoto,
-                    ip_address: ip_address
-                }
-            };
+            // this is a reply; set what comment it is replying to
+            newComment.replies_to_that = replyToId;
 
-            // find the comment that the reply is being made to and insert the id of the reply
+            // this comment is a reply
+            // find target comment and insert this comment ID
             insertResult = await commentsDB.updateOne(
                 { comment_id: replyToId },
                 { $push: { replies_to_this: comment_id } }
             );
 
-            console.log(`update comment replies_to_this: ${JSON.stringify(insertResult)}`);
-            // insert the reply into the database
-            insertResult = await commentsDB.insertOne(replyComment);
+            insertResult = await commentsDB.insertOne(newComment);
             console.log(`insert reply: ${JSON.stringify(insertResult)}`);
         } else {
-            // create a comment_id
-            const comment_id: string = `${article}_${username}_${receivedDatetime.getTime()}`;
+            // this is not a reply
+            newComment.replies_to_that = undefined;
 
-            // if comment is not a reply
-            const newComment: UserComment = {
-                comment_id: comment_id,
-                replies_to_this: [],
-                replies_to_that: undefined,
-                article: article,
-                comment: processedComment,
-                commentInfo: {
-                    datetime: receivedDatetime,
-                    likes: 0,
-                    dislikes: 0
-                },
-                userInfo: {
-                    email: email,
-                    username: username,
-                    profilePhoto: profilePhoto,
-                    ip_address: ip_address
-                }
-            };
-
-            // insert the comment into the database
             insertResult = await commentsDB.insertOne(newComment);
             console.log(`insert comment: ${JSON.stringify(insertResult)}`);
         }
