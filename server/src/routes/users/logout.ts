87fd800770f87ae8178ae502836ac2f3529fdbd7
dereckdemezquita <dereck@demezquita.com>
2023-02-21 00:48:42 +0000

    Added logout end point.

---
+++ server/src/routes/users/logout.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response } from 'express';
+import { sendRes } from '../../modules/helpers';
+
+export const logout = Router();
+
+// make sure to regenerate the session id when logging out
+logout.post('/users/logout', (req: Request, res: Response) => {
+    // if logout successful, send back a success message
+    req.session.destroy((err) => {
+        if (err) {
+            return sendRes(res, false, null, err.message);
+        }
+
+        return sendRes(res, true, "Successfully logged out!");
+    })
+});
