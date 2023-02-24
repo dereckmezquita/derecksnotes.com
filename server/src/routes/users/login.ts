9e5b585d0770e36a57cda02e06cdf8552fdd8e00
dereckdemezquita <dereck@demezquita.com>
2023-02-24 03:30:45 +0000

    Cleanup.

--- server/src/routes/users/login.ts
+++ server/src/routes/users/login.ts
@@ -1,11 +1,10 @@
 import { Router, Request, Response } from 'express';
-import { sendRes } from '../../modules/helpers';
 import { MongoClient } from 'mongodb';
+import * as argon2 from 'argon2';
 
+import { sendRes } from '../../modules/helpers';
 import { checkEmail } from '../../modules/validators';
 
-import * as argon2 from 'argon2';
-import { logger } from '../../modules/logger';
 
 export const login = Router();
 
@@ -23,60 +22,53 @@ export const initLogin = (client: MongoClient) => {
 
         // if they are already logged in and have an active session
         // sessions are saved to live memory
-        if ((req.session as SessionDataRes).authenticated) {
-            logger.info(`User ${email} is already logged in.`);
+        if ((req.session as SessionData).authenticated) {
             sendRes(res, true, `User ${email} is already logged in.`);
             return;
         }
 
+        // TODO: store previous user passwords
         const db = client.db('users');
-        const accounts = db.collection('accounts');
-        const user = await accounts.findOne({ "email.address": email }) as UserInfo | null;
+        const collection = db.collection('accounts');
+        const user = await collection.findOne<UserInfo>({ "email.address": email });
 
         // no match to that e-mail found
         if (!user) return sendRes(res, false, null, 'E-mail or password is incorrect');
+        if (!user.password) return sendRes(res, false, null, 'Do not have a valid password for user; please reset password.');
 
-        // compare the password received to the registered password
-        const passwordMatch = await argon2.verify(user.password, password, { type: argon2.argon2id, parallelism: 1 });
-
-        console.log(`passwordMatch result: ${passwordMatch}`);
+        // compare password received to registered password
+        const passwordCheck = await argon2.verify(
+            user.password, password, { type: argon2.argon2id, parallelism: 1 }
+        );
 
         // password does not match
-        if (!passwordMatch) return sendRes(res, false, null, 'E-mail or password is incorrect');
+        if (!passwordCheck) return sendRes(res, false, null, 'E-mail or password is incorrect');
 
         // password matches so we will modify the session object
-        (req.session as SessionDataRes).authenticated = true;
-        (req.session as SessionDataRes).user = { email: user.email.address, username: user.username, profilePhoto: user.profilePhoto } as UserCookie; // this is what gets saved client's cookies
+        (req.session as SessionData).authenticated = true;
+        (req.session as SessionData).user = { email: user.email.address, username: user.username, profilePhoto: user.profilePhoto } as SessionCookie; // this is what gets saved client's cookies
 
-        logger.info(`User ${user.email.address} has logged in.`);
         sendRes(res, true, "Successfully logged in!");
 
         // ----------------------------------------
         // update statistics in the database for the user
-        // get the users IP address
         const ip_address = req.headers['x-forwarded-for'] as string;
-
-        // if password matches update the user's last connected date
-        // check if the user has connected from this IP address before
         const ip_address_index = user.userStatistics.ip_addresses.findIndex((ip_address_obj) => ip_address_obj.ip_address === ip_address);
 
-        // if the user has connected from this IP address before
         if (ip_address_index !== -1) {
             // udpate the user's last connected date only for that IP address
-            logger.info(`User ${user.email.address} has connected from an existing IP address: ${ip_address}.`);
-            await accounts.updateOne(
+            await collection.updateOne(
                 { "email.address": email },
                 {
                     $set: { [`userStatistics.ip_addresses.${ip_address_index}.last_use`]: new Date() }
                 }
             );
-        } else {
-            logger.info(`User ${user.email.address} has connected from a new IP address: ${ip_address}.`);
-            user.userStatistics.ip_addresses.push({
-                ip_address: ip_address,
-                first_use: new Date(),
-                last_use: new Date()
-            });
         }
+
+        user.userStatistics.ip_addresses.push({
+            ip_address: ip_address,
+            first_use: new Date(),
+            last_use: new Date()
+        });
     });
 }
