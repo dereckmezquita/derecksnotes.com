434acd3a0e13daa7eb9203377be9d89bfdc6a047
dereckmezquita <dereck@mezquita.io>
2023-02-25 19:45:29 +0000

    Refactored more simple html constructor.

--- client/src/ts/modules/general/price_tickers.ts
+++ client/src/ts/modules/general/price_tickers.ts
@@ -1,5 +1,7 @@
+import { textToHTML } from "../helpers";
+
 type NameTicker = {
-    name: string,
+    coin_name: string,
     ticker: string
 }
 
@@ -53,87 +55,45 @@ const getPrice = async (coinName: string): Promise<PriceDataRes> => {
 
 async function setCryptoPrices(coins: NameTicker[]): Promise<void> {
     // get ticker holder from DOM
-    const tickers: Element = document.querySelector("#tickers");
+    const tickers: HTMLDivElement = document.querySelector("#tickers")!;
 
     for (const coin of coins) {
-        const res: PriceDataRes = await getPrice(coin.name);
+        const res: PriceDataRes = await getPrice(coin.coin_name);
+
+        console.log(res);
 
         if (!res) {
-            console.error(`Error fetching data for ${coin.name}`);
+            console.error(`Error fetching data for ${coin.coin_name}`);
             continue;
         }
 
-        const coinSpan: Element = document.createElement('span');
-        coinSpan.classList.add("info-bar-crypto");
+        const formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }); // 23230.2 -> $23,230.20; 3241 -> $3,241.00
 
-        if (coin.name != "bitcoin") { // css will hide all these when screen width is small
-            coinSpan.classList.add("info-bar-crypto-hidden");
-        }
+        const price: string = formatter.format(res.usd);
+        const pctChg: number = res.usd_24h_change;
 
-        // --------------------------------
-        // coin ticker
-        const coinTicker: Element = document.createElement('span');
-        coinTicker.innerHTML = coin.ticker + ": ";
-        coinTicker.id = `${coin.ticker.toLowerCase()}-name`;
+        const coinTicker = textToHTML(`
+        <span class="info-bar-crypto ${coin.ticker.toLowerCase() !== "btc" ? ' info-bar-crypto-hidden' : ''}">
+            <span id="${coin.ticker.toLowerCase()}-name">${coin.ticker}: ${price}</span>
+            <span id="${coin.ticker.toLowerCase()}-percent" class="coin-percent-change" style="background:${pctChg > 0 ? '#4DD964' : '#FF3B2F'};">
+                ${pctChg.toFixed(2)}%
+            </span>
+        </span>
+        `) as HTMLSpanElement;
 
-        // --------------------------------
-        // coin price
-        const coinPrice: Element = document.createElement('span');
-        let price: string = res.usd
-            .toFixed(5)
-            .toString();
-
-        // remove trailing zeros with a regular expression; toFixed adds zeros
-        price = price.replace(/0+$/, "").replace(/\.$/, "");
-
-        // if nothing after decimals add .00
-        if (+price % 1 === 0) price = price + ".00";
-
-        // split price by . or ,
-        const priceSplit: string[] = price.split(/[.,]/);
-        priceSplit[0] = priceSplit[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-        price = priceSplit[0] + "." + priceSplit[1];
-
-        coinPrice.innerHTML = "$ " + price;
-        coinPrice.id = `${coin.ticker.toLowerCase()}-price`;
-
-        // --------------------------------
-        // percent change
-        const coinPercentChange: HTMLElement | null = document.createElement('span');
-
-        let percentChangeValue: number = res.usd_24h_change;
-
-        if (percentChangeValue > 0) {
-            coinPercentChange.innerHTML = "+" + (percentChangeValue as number).toFixed(2) + "%";
-            coinPercentChange.style.background = "#4DD964";
-        } else {
-            coinPercentChange.innerHTML = (percentChangeValue as number).toFixed(2) + "%";
-            coinPercentChange.style.background = "#FF3B2F";
-        }
-
-        coinPercentChange.id = `${coin.ticker.toLowerCase()}-percent`;
-        coinPercentChange.classList.add("coin-percent-change");
-
-        // --------------------------------
-        // --------------------------------
-        // append to coin holder
-        coinSpan.appendChild(coinTicker);
-        coinSpan.appendChild(coinPrice);
-        coinSpan.appendChild(coinPercentChange);
-
-        // append to DOM
-        tickers.appendChild(coinSpan);
+        tickers.appendChild(coinTicker);
     }
 };
 
 const coins: NameTicker[] = [
-    { name: "bitcoin", ticker: "BTC" },
+    { coin_name: "bitcoin", ticker: "BTC" },
     // { name: "digibyte", ticker: "DGB" },
-    { name: "monero", ticker: "XMR" }
+    { coin_name: "monero", ticker: "XMR" }
 ]
 
 setCryptoPrices(coins);
 
 setInterval(() => {
+    document.querySelectorAll(".info-bar-crypto").forEach(e => e.remove());
     setCryptoPrices(coins);
 }, refreshInterval);
