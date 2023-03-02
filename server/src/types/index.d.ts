8c8fa59a17f42c00d98e170c71eb4952474565a5
dereckdemezquita <dereck@demezquita.com>
2023-03-02 16:29:30 +0000

    Tightening types.

--- server/src/types/index.d.ts
+++ server/src/types/index.d.ts
@@ -1,21 +1,6 @@
-export { };
-
-import { SessionData } from "express-session";
+export {};
 
 declare global {
-    // data stored in (req.session as SessionDataRes).user
-    type SessionCookie = {
-        email: string,
-        username: string,
-        profilePhoto?: string
-    }
-
-    // req.session
-    interface SessionData extends SessionData {
-        authenticated?: boolean;
-        user?: SessionCookie;
-    }
-
     // used by shared type GeoLocation
     type GeoLocateRes = {
         ip_address: string,
