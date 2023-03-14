60cfba617cd955f8cd4d9b7e95e27920c8fa506d
dereckdemezquita <dereck@demezquita.com>
2023-03-14 21:33:46 +0000

    Cleaning moved to shared.

--- server/src/types/index.d.ts
+++ server/src/types/index.d.ts
@@ -1,13 +1,3 @@
 export {};
 
-declare global {
-    // used by shared type GeoLocation
-    type GeoLocateRes = {
-        ip_address: string,
-        country: string,
-        regionName: string,
-        city: string,
-        isp: string,
-        org: string
-    }
-}
+declare global { }
