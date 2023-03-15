727bc06b168c432f4479be6d1b7437d72084c458
dereckmezquita <dereck@mezquita.io>
2023-03-15 21:23:13 +0000

    Rendering comments.

--- client/src/ts/modules/articles/user_comments.ts
+++ client/src/ts/modules/articles/user_comments.ts
@@ -3,7 +3,6 @@ import { dateToString, textToHTML } from "../helpers";
 
 // TODO: consider breaking up code: https://stackoverflow.com/questions/12706290/typescript-define-class-and-its-methods-in-separate-files
 
-
 class CommentSectionHandler {
     private contentWrapper = document.querySelector(".content-wrapper") as HTMLDivElement; // wraps article
     private userInfo: UserInfo; // response from server set on class instantiation
@@ -15,7 +14,7 @@ class CommentSectionHandler {
         return `
         <div class="new-comment-form ${reply ? "top-level-comment-form" : "reply-level-comment-form"}">
             <div class="comment-user-info">
-                <span class="username">${this.userInfo.username}</span>
+                <span class="username">${this.userInfo.username} ${this.userInfo.metadata.geo_locations[0].flag}</span>
                 <span></span>
                 <button class="comment-action">Post</button>
             </div>
@@ -51,16 +50,18 @@ class CommentSectionHandler {
         this.contentWrapper.insertAdjacentHTML("beforeend", this.commentSection);
 
         // this.setUserInfo();
-        this.addNewcommentListeners();
+        this.addNewCommentListeners();
         this.getComments();
     }
 
-    private addNewcommentListeners() {
+    private addNewCommentListeners() {
         // add event listener to the post button
         document.querySelector(".new-comment-form .comment-action")!.addEventListener("click", async () => {
             // get the value of the textarea
             const textarea: HTMLTextAreaElement = document.querySelector(".new-comment-form textarea")!;
             const comment: string = textarea.value.trim();
+
+            if (comment.length === 0) alert("Comment cannot be empty");
 
             // clear the textarea
             textarea.value = "";
@@ -93,54 +94,52 @@ class CommentSectionHandler {
         const maxCommentLength: number = 300;
 
         for (const comment of commentsRes) {
-            this.currentComment = comment;
-
-            const commentElement = this.generateComment(maxCommentLength);
+            const commentElement = this.renderComment(comment, maxCommentLength);
 
             // postedCommentsDiv.insertAdjacentHTML("beforeend", commentElement);
             postedCommentsDiv.appendChild(commentElement);
         }
     }
 
-    private generateComment(maxCommentLength: number): HTMLDivElement {
+    private renderComment(userComment: UserComment, maxCommentLength: number): HTMLDivElement {
         // convert datetime to this format 2022-01-01 12:00
-        const currentDatetime: string = dateToString(new Date(this.currentComment.commentInfo.datetime));
+        const datetime: string = dateToString(new Date(userComment.metadata.datetime));
 
-        let comment: string = this.currentComment.comment;
-        if (this.currentComment.comment.length > maxCommentLength) {
-            comment = this.currentComment.comment.slice(0, maxCommentLength) + `<span style="display: none;" class="comment-hidden-text">${this.currentComment.comment.slice(maxCommentLength)}</span>` + ` <a class="read-more-comment">read more...</a><a style="display: none;" class="read-less-comment">...read less.</a>`;
+        let comment: string = userComment.comment;
+        if (userComment.comment.length > maxCommentLength) {
+            comment = userComment.comment.slice(0, maxCommentLength) + `<span style="display: none;" class="comment-hidden-text">${userComment.comment.slice(maxCommentLength)}</span>` + ` <a class="read-more-comment" style="cursor: pointer;">read more...</a><a style="display: none;" class="read-less-comment">...read less.</a>`;
         }
 
-        console.log(this.currentComment);
-
         const commentElement = textToHTML(`
-        <div id="${this.currentComment.comment_id}" class="posted-comment">
+        <div id="${userComment.comment_id}" class="posted-comment">
             <div class="comment-user-info">
-                <span class="username">${this.currentComment.userInfo.username}</span>
-                <span class="datetime">${currentDatetime}</span>
+                <span class="username">${userComment.metadata.user.username} ${userComment.metadata.geo_location.flag}</span>
+                <span class="datetime">${datetime}</span>
                 <button class="comment-action comment-action-reply">Reply</button>
                 <button class="comment-action comment-action-report">Report</button>
             </div>
             <div class="posted-comment-holder">
-                <img src="${this.currentComment.userInfo.profilePhoto}" class="comment-profile-photo">
+                <img src="${userComment.metadata.user.profilePhoto}" class="comment-profile-photo">
                 <div class="posted-comment-text">${comment}</div>
             </div>
             <div class="posted-comment-actions">
                 <button class="like-button">Like</button>
-                <span class="like-count">${this.currentComment.commentInfo.likes}</span>
+                <span class="like-count">${userComment.metadata.likes}</span>
                 <button class="dislike-button">Dislike</button>
-                <span class="dislike-count">${this.currentComment.commentInfo.dislikes}</span>
+                <span class="dislike-count">${userComment.metadata.dislikes}</span>
             </div>
         </div>`) as HTMLDivElement;
 
         // add listener for show more button for that specific comment
-        if (this.currentComment.comment.length > maxCommentLength) {
+        if (userComment.comment.length > maxCommentLength) {
             const readMore = commentElement.querySelector(".read-more-comment") as HTMLAnchorElement;
             const readLess = commentElement.querySelector(".read-less-comment") as HTMLAnchorElement;
             const hiddenText = commentElement.querySelector(".comment-hidden-text") as HTMLSpanElement;
 
             readMore.addEventListener("click", () => {
                 readMore.style.display = "none";
+                readLess.style.display = "inline";
+                readLess.style.cursor = "pointer";
                 hiddenText.style.display = "inline";
 
                 // add event listener to read less button
@@ -148,6 +147,7 @@ class CommentSectionHandler {
                     hiddenText.style.display = "none";
                     readMore.style.display = "inline";
                     readLess.style.display = "none";
+                    readLess.style.cursor = "pointer";
                 });
             });
         }
@@ -156,11 +156,9 @@ class CommentSectionHandler {
     }
 }
 
-
 (async () => {
     const res: ServerRes<UserInfo> = await getUserInfo();
 
-    console.log(res.data);
     if (!res.success || !res.data) throw new Error(res.error);
 
     new CommentSectionHandler(res.data);
