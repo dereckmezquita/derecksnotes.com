01b9f8fec103377b4551e65756267ff622ee2f95
dereckmezquita <dereck@mezquita.io>
2023-03-15 19:29:33 +0000

    Moved to unifed db, need to await.

--- server/src/routes/users/get_user_info.ts
+++ server/src/routes/users/get_user_info.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { sendRes } from '../../modules/helpers';
-import { MongoClient } from 'mongodb';
+import { Db } from 'mongodb';
 
 import { geoLocate } from '../../modules/geoLocate';
 
@@ -19,7 +19,7 @@ export const get_user_info = Router();
 // 3. get/send the user's info from the database; exclude password and only get the 10 most recent ip addresses
 //     3.1 if the user is not found in the database send an error
 // 5. update the user's last_connected date and geo_location if necessary
-export const init_get_user_info = (client: MongoClient) => {
+export const init_get_user_info = (db: Db) => {
     get_user_info.post('/users/get_user_info', async (req: Request, res: Response) => {
         // 1. get the user's ip address and create a new date object
         const ip_address = req.headers['x-forwarded-for'] as string;
@@ -44,31 +44,32 @@ export const init_get_user_info = (client: MongoClient) => {
                 }
             }
 
-            console.log("User not loggedin; sending default user info.")
+            console.log("User not logged in; sending default user info.")
             return sendRes(res, true, defaultUser);
         }
 
         const cookie = (req.session as SessionData).user as SessionCookie;
-        const email = cookie.email;
+        const email = cookie.email.address;
         const username = cookie.username;
 
-        const db = client.db('users');
-        const accounts = db.collection('accounts');
+        console.log(`User logged in; sending user info for ${username} (${email})`)
+
+        const accounts = db.collection('user_accounts');
 
         // 3. get/send the user's info from the database; exclude password and only get the 10 most recent ip addresses
-        const userInfo = accounts.findOne<UserInfo>(
+        const userInfo = await accounts.findOne<UserInfo>(
             { "email.address": email, username: username },
             { projection: {
                 password: 0,
                 ip_addresses: { $slice: -10 }
             }}
         );
+
         if (!userInfo) return sendRes(res, false, null, 'User not found in database.');
+
         sendRes(res, true, userInfo);
 
         // 5. update the user's last_connected date and geo_location if necessary
         // const idx = userInfo.metadata.geo_location.findIndex((gl) => gl.ip_address === ip_address);
     });
 }
-
-// `/site-images/user-content/profile-photos/${username}.jpg`
