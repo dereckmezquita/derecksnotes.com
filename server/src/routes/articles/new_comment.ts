9f9c00ed823aa2bb050c0b42ee046fd98fc38a8a
dereckmezquita <dereck@mezquita.io>
2023-03-17 22:15:12 +0000

    Mentions added process comment to include user profile links.

--- server/src/routes/articles/new_comment.ts
+++ server/src/routes/articles/new_comment.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
 import { generateHashID, sendRes } from '../../modules/helpers';
 import { Db } from 'mongodb';
-import { userCommentCheck } from '../../modules/validators';
+import { userCommentCheck, checkUsername } from '../../modules/validators';
 import { geoLocate } from '../../modules/geoLocate';
 
 import sanitizeHtml from 'sanitize-html';
@@ -29,7 +29,7 @@ export const init_new_comment = (db: Db) => {
         const profilePhoto: string | undefined = cookie.profilePhoto;
 
         // ------------------------------------
-        const { comment, datetime, article, replyToId } = (req.body as { comment: string, datetime: string, article: string, replyToId?: string });
+        const { comment, datetime, article, mentions, replyToId } = (req.body as { comment: string, datetime: string, article: string, replyToId?: string, mentions: string[] });
 
         if (typeof comment !== "string") return sendRes(res, false, null, "Comment must be valid string.");
         
@@ -48,6 +48,7 @@ export const init_new_comment = (db: Db) => {
         // check that the comment is not empty and alphanumeric
         const commentCheck: { success: boolean, error?: string } = userCommentCheck(comment);
         if (!commentCheck.success) return sendRes(res, false, null, commentCheck.error);
+        // ------------------------------------
 
         const allowedTags: string[] = [
             "h1", "h2", "h3", "span", "strong", "p", "em", "a",
@@ -69,6 +70,42 @@ export const init_new_comment = (db: Db) => {
         // processedComment = processedComment.trim().replace(/[\r\n]+/g, '\n'); //.replace(/[\r\n]{3,}/g, '\n\n');
 
         // ------------------------------------
+        // process the comment and insert links to mentions
+        // first check that each mention does in fact mention a real user in our database
+        const usersDB = db.collection('user_accounts');
+
+        // TODO: this is an attack point; user could send a request with a lot of mentions and crash the server
+        // check each mention is a valid username if not valid
+        for (let i = 0; i < mentions.length; i++) {
+            console.log(`checking mention ${mentions[i]}`)
+            const mentionCheck: { success: boolean, error?: string } = checkUsername(mentions[i]);
+
+            // if not valid remove it from the mentions array
+            if (!mentionCheck.success) {
+                mentions.splice(i, 1);
+                i--;
+                continue;
+            }
+
+            const mentionedUser = await usersDB.findOne({ username: mentions[i] });
+            console.log(await usersDB.findOne({ username: "dereckmezquita" }));
+
+            console.log(`mentioned user: ${mentionedUser}`)
+
+            // if we find the user in our database then we can insert a link to their profile
+            if (mentionedUser) {
+                const link = `<a class="username username-mention" href="/user/${mentions[i]}">@${mentions[i]}</a>`;
+                console.log(`link: ${link}`)
+                processedComment = processedComment.replace(`@${mentions[i]}`, link);
+                console.log(`processed comment after adding link: ${processedComment}`)
+            } else {
+                // if we don't find the user in our database then we remove it from the mentions array
+                mentions.splice(i, 1);
+                i--;
+            }
+        }
+        console.log(`mentions after processing: ${mentions}`)
+        // ------------------------------------
         const commentsDB = db.collection('article_comments');
 
         // get the user's current IP address
@@ -81,8 +118,9 @@ export const init_new_comment = (db: Db) => {
         // create comment object; will be modified and inserted
         const newComment: UserComment = {
             comment_id: comment_id,
-            replies_to_this: [],
+            replies_to_this: [], // 0 length array because we want to be able to push into it
             replies_to_that: undefined,
+            mentions: mentions,
             article: article,
             comment: processedComment,
             metadata: {
