3e7e56e46aef162331bf919fc2c1dff76ffa9f1e
dereckdemezquita <dereck@demezquita.com>
2023-03-20 15:13:39 +0000

    Refined.

--- server/src/routes/articles/judge_comment.ts
+++ server/src/routes/articles/judge_comment.ts
@@ -31,29 +31,51 @@ export const init_judge_comment = (db: Db) => {
         if (!user) return sendRes(res, false, "User not found.");
 
         let hasVoted = false;
-        // check if the user has already voted on this comment
-        if (user.metadata.commentsJudged) {
-            hasVoted = user.metadata.commentsJudged.some((comment) => {
-                return comment.comment_id === commentId;
-            });
+        const existingJudgement = user.metadata.commentsJudged?.find((judgedComment) => {
+            return judgedComment.comment_id === commentId;
+        });
+
+        if (existingJudgement) {
+            hasVoted = true;
+            if (existingJudgement.judgement === vote) {
+                // If the user is trying to vote again with the same judgement, we can simply return an error message.
+                return sendRes(res, false, "You have already voted with the same judgement.");
+            }
         }
 
         // update comment
         if (!hasVoted) {
-            // if the user has not voted on this comment inc by 1
+            // if the user has not voted on this comment, increment by 1
             await commentsDB.updateOne({ comment_id: commentId }, {
                 $inc: {
                     [vote]: 1
                 }
             });
+
+            // Add the new judgement to the user's metadata
+            await usersDB.updateOne({ username: username }, {
+                $push: {
+                    "metadata.commentsJudged": {
+                        comment_id: commentId,
+                        judgement: vote
+                    }
+                }
+            });
         } else {
-            // if the user has voted on this comment we are toggling the vote
+            // if the user has voted on this comment, we are toggling the vote
             await commentsDB.updateOne({ comment_id: commentId }, {
                 $inc: {
                     // increase by 1
                     [vote]: 1,
                     // decrease the other vote by 1
                     [vote === "like" ? "dislike" : "like"]: -1
+                }
+            });
+
+            // Update the existing judgement in the user's metadata
+            await usersDB.updateOne({ username: username, "metadata.commentsJudged.comment_id": commentId }, {
+                $set: {
+                    "metadata.commentsJudged.$.judgement": vote
                 }
             });
         }
