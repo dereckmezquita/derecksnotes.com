57b653120faafc40e0e125bc8c507faccb5bcb63
dereckdemezquita <dereck@demezquita.com>
2023-03-22 17:44:29 +0000

    React project files.

---
+++ client-react/src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+  /* Add styles for header */
+`;
+
+const Header: React.FC = () => {
+  return (
+    <HeaderContainer>
+      <a href="/index.html" className="home-page-link">
+        <span id="header-established">Established France 2017</span>
+        <span id="header-slogan">
+          The online brain of Dereck <span id="header-author-particule">de </span>Mezquita
+        </span>
+        <span className="header-site-name">Dereck's Notes</span>
+      </a>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
