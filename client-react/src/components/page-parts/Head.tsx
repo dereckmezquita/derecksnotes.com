61b67c6fc1951e38459523264393d22ba897eacd
dereckdemezquita <dereck@demezquita.com>
2023-03-24 16:34:23 +0000

    Function allows pass title.

--- client-react/src/components/page-parts/Head.tsx
+++ client-react/src/components/page-parts/Head.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-// TODO: use helmet and make this a function so dynamic set title and content summary
-const Head: React.FC = () => {
+interface HeadProps {
+    title?: string;
+}
+
+const Head: React.FC<HeadProps> = ({ title }) => {
+    const defaultTitle = "Dereck's Notes";
+
     return (
         <Helmet>
-            <title>Dereck's Notes</title>
+            <title>{title ? `${defaultTitle} | ${title}` : defaultTitle}</title>
             <meta
                 name="description"
-                content="The online brain of Dereck de Mezquita; making sciencing easier."
+                content="The online brain of Dereck Mezquita; making sciencing easier."
             />
             <meta
                 name="keywords"
                 content="Science, Programming, Bioinformatics, Biology, Technology, Education, Art, Dictionary, Blog"
             />
-            {/* Add other meta tags and head elements here */}
+            <meta name="author" content="Dereck Mezquita" />
+
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Helmet>
     );
 };
