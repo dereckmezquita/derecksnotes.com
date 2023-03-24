a745379f31dc8284e0ea61bf50fbd1ae7a9e9afe
dereckdemezquita <dereck@demezquita.com>
2023-03-24 15:53:29 +0000

    Using helmet for setting head tags.

--- client-react/src/components/page-parts/Head.tsx
+++ client-react/src/components/page-parts/Head.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
+// TODO: use helmet and make this a function so dynamic set title and content summary
 const Head: React.FC = () => {
-  return (
-    <>
-      <title>Dereck's Notes</title>
-      <meta
-        name="description"
-        content="The online brain of Dereck de Mezquita; making sciencing easier."
-      />
-      <meta
-        name="keywords"
-        content="Science, Programming, Bioinformatics, Biology, Technology, Education, Art, Dictionary, Blog"
-      />
-      {/* Add other meta tags and head elements here */}
-    </>
-  );
+    return (
+        <Helmet>
+            <title>Dereck's Notes</title>
+            <meta
+                name="description"
+                content="The online brain of Dereck de Mezquita; making sciencing easier."
+            />
+            <meta
+                name="keywords"
+                content="Science, Programming, Bioinformatics, Biology, Technology, Education, Art, Dictionary, Blog"
+            />
+            {/* Add other meta tags and head elements here */}
+        </Helmet>
+    );
 };
 
 export default Head;
