e1f95a74abc93a550a28f135712687ca78df4072
dereckmezquita <dereck@mezquita.io>
2023-03-24 14:28:28 +0000

    Base blog page.

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,33 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
-import Head from './components/base/Head';
-import InfoBar from './components/base/InfoBar';
-import Header from './components/base/Header';
-import Nav from './components/base/Nav';
-import Footer from './components/base/Footer';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 
+import Head from './components/page-parts/Head';
+import InfoBar from './components/page-parts/InfoBar';
+import Header from './components/page-parts/Header';
+import Nav from './components/page-parts/Nav';
+import Footer from './components/page-parts/Footer';
 import CardList from './components/articles/CardList';
 
+import { GlobalStyle } from './styles/GlobalStyle.styles';
+
 const PageContainer = styled.div`
-  /* Add any global styles */
+    /* Add any global styles */
 `;
 
 const Wrapper = styled.div`
-  /* Add styles for .wrapper */
+    /* Add styles for .wrapper */
 `;
 
 const App: React.FC = () => {
     return (
-        <PageContainer>
-            <Head />
-            <InfoBar />
-            <Header />
-            <Nav />
-            <Wrapper>
-                <CardList siteSection="blog" />
-            </Wrapper>
-            <Footer />
-        </PageContainer>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <PageContainer>
+                <Head />
+                <InfoBar />
+                <Header />
+                <Nav />
+                <Wrapper>
+                    <CardList siteSection="blog" />
+                </Wrapper>
+                <Footer />
+            </PageContainer>
+        </ThemeProvider>
     );
 };
 
