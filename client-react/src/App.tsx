5fce358eb3ee94802b8a430cac70829c4bcaf694
dereckdemezquita <dereck@demezquita.com>
2023-07-09 23:41:01 +0000

    React port.

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,41 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme';
 
-import Head from './components/page-parts/Head';
-import InfoBar from './components/page-parts/InfoBar';
-import Header from './components/page-parts/Header';
-import Nav from './components/page-parts/Nav';
-import Footer from './components/page-parts/Footer';
-import CardList from './components/articles/CardList';
-
-import { GlobalStyle } from './styles/GlobalStyle.styles';
-
-const PageContainer = styled.div`
-    /* Add any global styles */
-`;
-
-const Wrapper = styled.div`
-    /* Add styles for .wrapper */
-`;
-
-const App: React.FC = () => {
+function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <PageContainer>
-                <Head />
-                <InfoBar />
-                <Header />
-                <Nav />
-                <Wrapper>
-                    <CardList siteSection="blog" />
-                </Wrapper>
-                <Footer />
-            </PageContainer>
-        </ThemeProvider>
+        <div></div>
     );
-};
+}
 
 export default App;
