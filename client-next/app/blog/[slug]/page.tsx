4e588ff6244b908d1ee788868066329884cae496
dereckmezquita <dereck@mezquita.io>
2023-07-30 09:48:59 -0500

    Processing blog content working well; now need to add code highlighting.

--- client-next/app/blog/[slug]/page.tsx
+++ client-next/app/blog/[slug]/page.tsx
@@ -1,9 +1,16 @@
 import fs from 'fs';
-import Markdown from 'markdown-to-jsx';
+import ReactMarkdown from 'react-markdown';
 import matter from 'gray-matter';
 import get_post_metadata from '@helpers/get_post_metadata';
 
-const getPostContent = (slug: string) => {
+import remarkGfm from 'remark-gfm'; // allows github flavored markdown
+import rehypeRaw from "rehype-raw"; // allows raw html in markdown
+import rehypeSlug from 'rehype-slug'; // adds id to headings
+import remarkUnwrapImages from 'remark-unwrap-images'; // removes p tag around images
+import remarkExternalLinks from 'remark-external-links'; // manage external links
+import remarkToc from 'remark-toc'
+
+const get_post_content = (slug: string) => {
     const folder = 'blog/';
     const file = `${folder}${slug}.md`;
     const content = fs.readFileSync(file, 'utf8');
@@ -11,23 +18,25 @@ const getPostContent = (slug: string) => {
 }
 
 // a next function to generate static paths for the dynamic pages
-export const generateStaticParams = () => {
+export const generateStaticParams = (): Array<{ slug: string }> => {
     const posts = get_post_metadata();
-    return posts.map((post) => {
+    return posts.map((post: PostMetadata) => {
         return { slug: post.slug }
     });
 }
 
 const PostPage = (props: any) => {
     const slug = props.params.slug;
-    const post = getPostContent(slug);
+    const post = get_post_content(slug);
 
     return (
         <>
-            <h1>{post.data.title}</h1>
-            <Markdown>
+            <ReactMarkdown
+                remarkPlugins={[remarkGfm, remarkUnwrapImages, remarkExternalLinks, remarkToc]}
+                rehypePlugins={[rehypeRaw, rehypeSlug]}
+            >
                 {post.content}
-            </Markdown>
+            </ReactMarkdown>
         </>
     )
 }
