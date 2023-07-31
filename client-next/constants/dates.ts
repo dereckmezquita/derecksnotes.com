32cc09cbc29c957d8b9fb4a122ea953acacc9006
dereckdemezquita <dereck@demezquita.com>
2023-07-30 19:51:35 -0500

    Misc.

---
+++ client-next/constants/dates.ts
@@ -0,0 +1,15 @@
+export const DATE_FORMAT = 'YYYY-MM-DD';
+
+export const TIME_FORMAT = 'HH:mm:ss';
+
+export const DATETIME_FORMAT = `${DATE_FORMAT} - ${TIME_FORMAT}`;
+
+export const MILLISECONDS_IN_ONE_DAY = 86400000;
+
+export enum DAYS_OF_WEEK {
+    MON = 0, TUE, WED, THU, FRI, SAT, SUN
+}
+
+export enum MONTHS_OF_YEAR {
+    JAN = 0, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
+}
