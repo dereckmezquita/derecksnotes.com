54f68feede997eadc57944779bcb67f433eca229
dereckmezquita <dereck@mezquita.io>
2023-08-26 17:43:55 -0500

    Now allows for different sections.

--- client-next/src/components/PostPreview.tsx
+++ client-next/src/components/PostPreview.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import path from 'path';
 
 const PostPreview = (props: PostMetadata) => {
     return (
         <div key={props.slug}>
-            <Link href={`/blog/${props.slug}`} passHref>
+            <Link href={path.join("/", props.section, props.slug)} passHref>
                 {props.title}
             </Link>
             <p>{props.subtitle}</p>
