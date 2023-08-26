a63142b6344d9df9ec74e8a9565378a38c67e3f4
dereckmezquita <dereck@mezquita.io>
2023-08-26 17:10:54 -0500

    Comment on ROOT.

--- client-next/src/constants/misc.ts
+++ client-next/src/constants/misc.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 
+// root starts at client-next/src
 export const ROOT = path.join(process.cwd(), 'src');
 export const APPLICATION_TITLE = 'derecksnotes.com';
 export const APPLICATION_AUTHOR: { first_name: string, last_name: string } = { first_name: 'Dereck', last_name: 'Mezquita' };
