5cac61421a5902c514eaf74135859c052b9fdca5
dereckmezquita <dereck@mezquita.io>
2023-08-26 17:44:18 -0500

    Working on article display component.

---
+++ client-next/src/components/Article.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { theme } from '@styles/theme';
+
+
+const Article = styled.article`
+    width: 60%;
+    margin: 0 auto;
+
+    /* background-color: ${theme.container.background.colour.content()}; */
+    background-color: yellow;
+`;
+
+export default Article;
