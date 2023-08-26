a3b0b150201de848f6775bba2109c8dbf2ff4b30
dereckdemezquita <dereck@demezquita.com>
2023-08-26 17:13:08 -0500

    Working on structure.

--- client-next/src/pages/_app.tsx
+++ client-next/src/pages/_app.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+
+// components
+import NavBar from '@components/ui/NavBar';
+import Logo from '@components/Logo';
+
+// styles
 import GlobalStyles from '@styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
-
-import NavBar from '@components/ui/NavBar';
-
 import { theme } from '@styles/theme';
+
+import '@public/fonts/roboto.css'; // sans-serif
+import '@public/fonts/tangerine.css'; // cursive
+import '@public/fonts/fjalla_one.css'; // block letters; main logo
 
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider theme={{ theme }}>
+        <ThemeProvider theme={ theme }>
             <GlobalStyles />
+            <Logo></Logo>
             <NavBar />
             <Component {...pageProps} />
         </ThemeProvider>
