e33b95351e4c4deead972469da1a69b45e74f56a
dereckdemezquita <dereck@demezquita.com>
2023-08-26 17:29:34 -0500

    Was having problems with styling on server side rendered components.

--- client-next/next.config.js
+++ client-next/next.config.js
@@ -8,7 +8,13 @@ const nextConfig = {
                 destination: 'https://derecksnotes.com/api/:path*'
             },
         ]
-    }
+    },
+    compiler: {
+        // https://stackoverflow.com/questions/67352231/why-all-styles-of-materialui-will-disappear-after-refresh-in-nextjs
+        // https://nextjs.org/docs/advanced-features/compiler
+        // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+        styledComponents: true,
+    },
 }
 
 module.exports = nextConfig
