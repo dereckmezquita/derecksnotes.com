c001a40da5e22e1641a255286ff09a8761b73063
dereckmezquita <dereck@mezquita.io>
2023-09-02 16:38:24 -0500

    Drop cap plugin working!

--- client-next/src/utils/markdown.ts
+++ client-next/src/utils/markdown.ts
@@ -97,18 +97,33 @@ interface DropCapConfig {
 
 function dropCap(config: DropCapConfig) {
     return (tree: Node) => {
-        // find the first paragraph node
+        // Step 1: Initialize firstParagraphNode
         let firstParagraphNode: Node | null = null;
-        visit(tree, 'element', (node: Node, index: number, parent: Node) => {
-            if (!firstParagraphNode && (node as any).tagName === 'p') {
-                firstParagraphNode = node;
-            }
-        });
 
+        // Step 2: Define findFirstParagraphNode function
+        const findFirstParagraphNode = (node: Node) => {
+            if (firstParagraphNode) return;
+
+            // Check if the current node is a paragraph element and contains text
+            if (node.type === 'element' && (node as any).tagName === 'p' && (node as any).children.some((child: any) => child.type === 'text' && child.value.trim() !== '')) {
+                firstParagraphNode = node;
+                return;
+            }
+            if ('children' in node) {
+                (node.children as Node[]).forEach(findFirstParagraphNode);
+            }
+        };
+
+        // Step 3: Call findFirstParagraphNode function
+        findFirstParagraphNode(tree);
+
+        // Step 4: Check if firstParagraphNode is found
         if (firstParagraphNode) {
             const firstChild = (firstParagraphNode as any).children[0];
             if (firstChild && firstChild.type === 'text') {
                 const value = firstChild.value.trim();
+
+                // Step 5: Create dropCapSpan
                 const dropCapSpan = {
                     type: 'element',
                     tagName: 'span',
@@ -128,6 +143,8 @@ function dropCap(config: DropCapConfig) {
                         value: value[0]
                     }]
                 };
+
+                // Step 6: Modify firstParagraphNode
                 firstChild.value = value.slice(1);
                 (firstParagraphNode as any).children.unshift(dropCapSpan);
             }
