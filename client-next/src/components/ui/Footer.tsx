000e151f3c3752b9de495977ba662994a40144d6
dereckmezquita <dereck@mezquita.io>
2023-09-03 14:50:20 -0500

    Footer component.

---
+++ client-next/src/components/ui/Footer.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { theme } from '@styles/theme';
+
+const FooterContainer = styled.footer`
+    margin: 0px auto;
+    padding: 0px;
+    text-align: center;
+
+    width: 50%;
+    border-top: 1px solid ${theme.text.colour.light_grey()};
+`;
+
+const FooterText = styled.p`
+    font-size: 0.85rem;
+    color: ${theme.text.colour.light_grey()};
+`;
+
+function Footer(): JSX.Element {
+    return (
+        <FooterContainer>
+            <FooterText>
+                All content on this website is copyright protected and belongs to Dereck Mezquita. You are not allowed to copy nor modify any content found on this website without written permission from the owner; &copy; Copyright © 2017 - 2023.
+            </FooterText>
+            <FooterText>
+                <a href="mailto:contact@derecksnotes.com">contact@derecksnotes.com</a>
+            </FooterText>
+        </FooterContainer>
+    );
+}
+
+export default Footer;
