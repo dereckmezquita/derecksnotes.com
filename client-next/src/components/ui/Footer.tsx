56857028d83b958ccf348178ae5c7ed35d1a2589
dereckmezquita <dereck@mezquita.io>
2023-09-03 21:29:41 -0500

    Duplicate C.

--- client-next/src/components/ui/Footer.tsx
+++ client-next/src/components/ui/Footer.tsx
@@ -20,7 +20,7 @@ function Footer(): JSX.Element {
     return (
         <FooterContainer>
             <FooterText>
-                All content on this website is copyright protected and belongs to Dereck Mezquita. You are not allowed to copy nor modify any content found on this website without written permission from the owner; &copy; Copyright © 2017 - 2023.
+                All content on this website is copyright protected and belongs to Dereck Mezquita. You are not allowed to copy nor modify any content found on this website without written permission from the owner; Copyright © 2017 - 2023.
             </FooterText>
             <FooterText>
                 <a href="mailto:contact@derecksnotes.com">contact@derecksnotes.com</a>
