5a0ef441f7a4444f08b53011f378732586b8018a
dereckmezquita <dereck@mezquita.io>
2023-09-08 17:24:23 -0500

    Added favicon.

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <GoogleAdsComponent />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <body>
                 <Main />
