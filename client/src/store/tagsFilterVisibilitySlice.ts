a37b240f2b6ed57a2683827528d3203299e9f9a4
dereckmezquita <dereck@mezquita.io>
2023-09-08 22:13:29 -0500

    Slice for controlling visibility of tags filter.

---
+++ client/src/store/tagsFilterVisibilitySlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface TagsFilterVisibilityState {
+    tagsFilterVisible: boolean;
+}
+
+const initialState: TagsFilterVisibilityState = {
+    tagsFilterVisible: false
+};
+
+export const tagsFilterVisibilitySlice = createSlice({
+    name: 'tagsFilterVisibility',
+    initialState,
+    reducers: {
+        toggleTagsFilter: state => {
+            state.tagsFilterVisible = !state.tagsFilterVisible;
+        }
+    }
+});
+
+export const { toggleTagsFilter } = tagsFilterVisibilitySlice.actions;
+export default tagsFilterVisibilitySlice.reducer;
