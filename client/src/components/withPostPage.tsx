37b703dd5554fe0bacbed2d35e82b2166c611eff
dereckmezquita <dereck@mezquita.io>
2023-09-09 09:24:31 -0500

    Added tags filter to sidebar.

--- client/src/components/withPostPage.tsx
+++ client/src/components/withPostPage.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
 import { theme } from '@styles/theme';
 import { SiteName } from '@components/ui/Logo';
+
+import TagFilter from '@components/ui/TagFilter';
+
+import { useSelector } from 'react-redux';
+import { RootState } from '@store/store';
 
 const PostContainer = styled.div`
     display: flex;
@@ -31,7 +36,7 @@ const SideBarContainer = styled.div`
         display: none;
     }
 `;
-const SideBarSiteName = styled(SiteName)<{ fontSize: string }>`
+const SideBarSiteName = styled(SiteName) <{ fontSize: string }>`
     font-size: 20px;
     border-bottom: 1px dashed ${theme.container.border.colour.primary()};
 `;
@@ -138,44 +143,76 @@ const PostContent = styled.div`
     }
 `;
 
+interface PostPageProps {
+    content: string;
+    post: PostMetadata;
+    posts: PostMetadata[];
+};
 
+// reminder this returns a function
+// this function is used in [slug].tsx and uses post, posts which are global variables
+// these global variables are set in getStaticProps
 export const withPostPage = (section: string) => {
-    const PostPage: React.FC<{
-        content: string; post: PostMetadata, postsMetadata: PostMetadata[]
-    }> = ({
-        content, post, postsMetadata
-    }) => {
-        // postsMetadata should already be sorted
+    const PostPage: React.FC<PostPageProps> = ({ content, post, posts }) => {
+        const allTags = Array.from(new Set(posts.flatMap(post => post.tags))).sort();
+
+        const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+        // if no tags selected, show all posts
+        const filteredPosts: PostMetadata[] = selectedTags.length > 0 ? posts.filter(
+            post => selectedTags.some(tag => post.tags.includes(tag))
+        ) : posts;
+
+        const handleTagSelect = (tag: string) => {
+            setSelectedTags(prev => [...prev, tag]);
+        };
+
+        const handleTagDeselect = (tag: string) => {
+            setSelectedTags(prev => prev.filter(t => t !== tag));
+        };
+
+        // redux control for tag filter visibility
+        const tagsFilterVisible = useSelector((state: RootState) => state.visibility.tagsFilterVisible);
+
         return (
-            <PostContainer>
-                <SideBarContainer>
-                    <SideBarSiteName fontSize='20px'>{`Dereck's Notes`}</SideBarSiteName>
-                    <SideBarEntriesContainer>
-                        {postsMetadata.map((postMetadata) => (
-                            <SideEntryLink
-                                key={postMetadata.slug}
-                                href={`/${section}/${postMetadata.slug}`}
-                                passHref
-                            >
-                                <span style={{ fontWeight: 'bold' }}>{postMetadata.date}</span>: {postMetadata.title}
-                            </SideEntryLink>
-                        ))}
-                    </SideBarEntriesContainer>
-                    <SideBarAboutContainer>
-                        <SideBarAboutH2>About</SideBarAboutH2>
-                        <p>
-                            This website is custom made by Dereck using React, Next.js, and TypeScript. It incorporates progressive web app technologies an relies on a NodeJS backend along with a MongoDB database.
-                        </p>
-                        <p>
-                            If you'd like to know more you can find the full source code on <a href='https://github.com/dereckmezquita/derecksnotes.com'>github.com/dereckmezquita/derecksnotes.com</a>
-                        </p>
-                    </SideBarAboutContainer>
-                </SideBarContainer>
-                <Article>
-                    <h1>{post.title}</h1>
-                    <PostContent dangerouslySetInnerHTML={{ __html: content }} />
-                </Article>
-            </PostContainer>
+            <>
+                <TagFilter
+                    tags={allTags}
+                    selectedTags={selectedTags}
+                    onTagSelect={handleTagSelect}
+                    onTagDeselect={handleTagDeselect}
+                    visible={tagsFilterVisible}
+                />
+                <PostContainer>
+                    <SideBarContainer>
+                        <SideBarSiteName fontSize='20px'>{`Dereck's Notes`}</SideBarSiteName>
+                        <SideBarEntriesContainer>
+                            {filteredPosts.map((meta) => (
+                                <SideEntryLink
+                                    key={meta.slug}
+                                    href={`/${section}/${meta.slug}`}
+                                    passHref
+                                >
+                                    <span style={{ fontWeight: 'bold' }}>{meta.date}</span>: {meta.title}
+                                </SideEntryLink>
+                            ))}
+                        </SideBarEntriesContainer>
+                        <SideBarAboutContainer>
+                            <SideBarAboutH2>About</SideBarAboutH2>
+                            <p>
+                                This website is custom made by Dereck using React, Next.js, and TypeScript. It incorporates progressive web app technologies an relies on a NodeJS backend along with a MongoDB database.
+                            </p>
+                            <p>
+                                If you'd like to know more you can find the full source code on <a href='https://github.com/dereckmezquita/derecksnotes.com'>github.com/dereckmezquita/derecksnotes.com</a>
+                            </p>
+                        </SideBarAboutContainer>
+                    </SideBarContainer>
+                    <Article>
+                        <h1>{post.title}</h1>
+                        <PostContent dangerouslySetInnerHTML={{ __html: content }} />
+                    </Article>
+                </PostContainer>
+            </>
         );
     };
 
