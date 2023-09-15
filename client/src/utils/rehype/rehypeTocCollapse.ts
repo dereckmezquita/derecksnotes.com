08ea70c8eda24b7b50a2528b0611bc2439b3221a
dereckmezquita <dereck@mezquita.io>
2023-09-15 15:21:09 -0500

    Working version depends on remarkToc.

--- client/src/utils/rehype/rehypeTocCollapse.ts
+++ client/src/utils/rehype/rehypeTocCollapse.ts
@@ -4,36 +4,46 @@ import { visit } from 'unist-util-visit';
 
 const rehypeTocCollapse: Plugin = () => {
     return (tree) => {
-        // we want to visit nav element that has class toc
         visit(tree, 'element', (node: Element, index, parent: any) => {
-            if (node.tagName === 'nav' && node.properties && node.properties.className && (node.properties.className as string[]).includes('toc')) {
-                if (!parent || !Array.isArray(parent.children)) return;
+            if (typeof index !== 'number') return;
 
-                // Replace the TOC with a collapsible element
-                parent.children.splice(index, 1, {
-                    type: 'element',
-                    tagName: 'details',
-                    properties: {},
-                    children: [
-                        {
-                            type: 'element',
-                            tagName: 'summary',
-                            properties: {},
-                            children: [
-                                {
-                                    type: 'element',
-                                    tagName: 'h2',
-                                    properties: {
-                                        id: 'table-of-contents',
-                                        style: 'display: inline-block; margin: 0px; padding: 0px; padding-bottom: 15px;'
-                                    },
-                                    children: [{ type: 'text', value: 'Table of Contents' }]
-                                }
-                            ]
-                        },
-                        node,
-                    ]
-                });
+            if (node.properties?.id === 'table-of-contents') {
+                if (!parent || !Array.isArray(parent.children) || index === parent.children.length - 1) return;
+                const subsequentNode = parent.children[index + 2];
+
+                if (subsequentNode.tagName === 'ul') {
+                    // Remove the original h1 (Table of Contents title)
+                    parent.children.splice(index, 1);
+
+                    // Replace the subsequentNode (which is the TOC) with a collapsible element
+                    parent.children.splice(index, 1, {
+                        type: 'element',
+                        tagName: 'details',
+                        properties: {},
+                        children: [
+                            {
+                                type: 'element',
+                                tagName: 'summary',
+                                properties: {},
+                                children: [
+                                    {
+                                        type: 'element',
+                                        tagName: 'h2',
+                                        properties: {
+                                            id: 'table-of-contents',
+                                            style: 'display: inline-block; margin: 0px; padding: 0px; padding-bottom: 10px;'
+                                        },
+                                        children: [{ type: 'text', value: 'Table of Contents' }]
+                                    }
+                                ]
+                            },
+                            subsequentNode
+                        ]
+                    });
+
+                    // Remove the original ul TOC
+                    parent.children.splice(index + 1, 1);
+                }
             }
         });
     };
