97fe137f2750aff5968d0eaf98bf49541875ed34
dereckmezquita <dereck@mezquita.io>
2023-09-18 18:05:28 -0500

    Get dictionaries.

--- dev/mongo-commands.sh
+++ dev/mongo-commands.sh
@@ -1,2 +1,4 @@
 # dump collection to a json file
 mongoexport --db=derecksnotes --collection=articles_metadata --out=articles_metadata.json
+
+mongoexport --db=derecksnotes --collection=dictionary_definitions --out=dictionary_definitions.json
