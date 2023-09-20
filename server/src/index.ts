52839f9c7f56282286a8d444dd33d34b2104adcd
dereckmezquita <dereck@mezquita.io>
2023-09-19 19:48:00 -0500

    Minimal db working hello.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,69 +1,31 @@
 import express from 'express';
-import session from 'express-session';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectToDB } from './utils/mongoConnect';
+import authRoutes from './routes/authRoutes';
 
-import { router, initDB } from './routes';
-import { MongoClient } from 'mongodb';
-import { logger } from './modules/logger';
-
-import { createClient } from 'redis';
-import makeRedisStore from 'connect-redis';
-
-// redis client for managing sessions
-const redisClient = createClient({legacyMode: true});
-
-const redisStore = makeRedisStore(session);
-
-const port = Number(process.env.PORT) || 3002;
-const mongodbUrl = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017';
+dotenv.config({ path: '../.env' });
 
 const app = express();
 
-// TODO: add rate limiter with express for longer term protection (100 requests per day or something)
-// use ratelimiter on express side to limit request per day
-// nginx is being used for burst and spam protection
+if (process.env.NODE_ENV === 'development') app.use(cors());
 
-// --------------------------------
-// middleware
-// --------------------------------
 app.use(express.json());
 
-// --------------------------------
-async function start() {
-    await redisClient.connect();
-    // Add the express-session middleware
-    app.use(
-        session({
-            store: new redisStore({ client: redisClient }),
-            secret: 'temporary-secret', // TODO: load this from ignored module to be secret
-            resave: false,
-            saveUninitialized: false,
-            // days * hours * minutes * seconds * milliseconds
-            cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // let's make the session last for 30 days
-        })
-    );
+// ------------------
+const PORT = 3001;
 
-    app.use(router);
 
-    // middleware to log rate limiter status
-    app.use((req, res, next) => {
-        if (req.route && req.route.path === '/api/' && res.statusCode === 429) {
-            logger.warn(`Rate limit exceeded for IP ${req.ip}`);
-        }
 
-        next();
-    });
+connectToDB();
 
-    const client = await MongoClient.connect(mongodbUrl, {
-        serverSelectionTimeoutMS: 1000,
-    });
+// ------------------
+app.use('/api/auth', authRoutes);
+app.get('/api/hello', (req, res) => {
+    res.send('Hello World!');
+});
 
-    const db = client.db('derecksnotes_test');
-
-    initDB(db);
-
-    app.listen(port, '0.0.0.0', () => {
-        logger.info(`Server is up on port: ${port}`);
-    });
-}
-
-start().catch(console.error);
+// ------------------
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
