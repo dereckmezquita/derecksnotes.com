f070f049d290b571faaffb982f2dc18597f4b570
dereckmezquita <dereck@mezquita.io>
2023-09-23 13:30:36 -0500

    Added user slice and thunk to async get user data on app load, save to redux state.

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tagsFilterVisibilitySlice from './tagsFilterVisibilitySlice';
+import userSlice from './userSlice';
+
 
 export const store = configureStore({
     reducer: {
-        visibility: tagsFilterVisibilitySlice // toggle post tags filter visibility
-    }
+        visibility: tagsFilterVisibilitySlice, // toggle post tags filter visibility
+        user: userSlice // user data
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
 });
 
 export type RootState = ReturnType<typeof store.getState>;
---
+++ client/src/store/userSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface UserState {
+    isAuthenticated: boolean;
+    data: any | null; // Update this 'any' to the actual type of your user data if available
+    loading: boolean;
+    error: string | null;
+}
+
+const initialState: UserState = {
+    isAuthenticated: false,
+    data: null,
+    loading: false,
+    error: null
+};
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        fetchUserDataStart: (state) => {
+            state.loading = true;
+        },
+        fetchUserDataSuccess: (state, action: PayloadAction<any>) => {
+            state.loading = false;
+            state.isAuthenticated = true;
+            state.data = action.payload;
+        },
+        fetchUserDataFailure: (state, action: PayloadAction<string>) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        logout: (state) => {
+            return initialState;
+        }
+    }
+});
+
+export const { fetchUserDataStart, fetchUserDataSuccess, fetchUserDataFailure, logout } = userSlice.actions;
+export default userSlice.reducer;
