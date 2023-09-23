1a3ea5de41e4294800189c853f4b1e2b609a3caf
dereckmezquita <dereck@mezquita.io>
2023-09-23 13:30:36 -0500

    Added user slice and thunk to async get user data on app load, save to redux state.

---
+++ client/src/store/userThunks.ts
@@ -0,0 +1,14 @@
+import { fetchUserDataStart, fetchUserDataSuccess, fetchUserDataFailure } from './userSlice';
+import api_me from '@utils/api/me';
+import { AppDispatch } from './store';
+
+export const fetchUserData = () => async (dispatch: AppDispatch) => {
+    dispatch(fetchUserDataStart());
+
+    try {
+        const userData = await api_me();
+        dispatch(fetchUserDataSuccess(userData));
+    } catch (error: any) {
+        dispatch(fetchUserDataFailure(error.message));
+    }
+};
