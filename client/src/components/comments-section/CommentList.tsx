3c88f016ca0ab10e2e1a70767129977a15143668
dereckmezquita <dereck@mezquita.io>
2023-09-26 16:54:52 -0500

    Work in progress.

--- client/src/components/comments-section/CommentList.tsx
+++ client/src/components/comments-section/CommentList.tsx
@@ -6,20 +6,29 @@ const ListContainer = styled.div`
     /* Add any desired styling for the container of the comments here */
 `;
 
+interface User {
+    id: string;
+    username: string;
+    profileImage: string;
+}
+
 interface UserComment {
     id: string;
     text: string;
+    author: User;
+    replies: UserComment[];
 }
 
 interface CommentListProps {
     comments: UserComment[];
+    currentUserId: string;
 }
 
-const CommentList: React.FC<CommentListProps> = ({ comments }) => {
+const CommentList: React.FC<CommentListProps> = ({ comments, currentUserId }) => {
     return (
         <ListContainer>
             {comments.map(comment => (
-                <Comment key={comment.id} comment={comment} />
+                <Comment key={comment.id} comment={comment} userId={currentUserId} />
             ))}
         </ListContainer>
     );
