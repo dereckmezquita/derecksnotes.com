1fc6ad795ce9d0fe6a41cc3633d08113488a5e08
dereckmezquita <dereck@mezquita.io>
2023-09-26 16:48:59 -0500

    Refined schemas with defaults.

--- server/src/models/CommentInfo.ts
+++ server/src/models/CommentInfo.ts
@@ -1,19 +1,27 @@
 import mongoose from "mongoose";
 
 const commentInfoSchema = new mongoose.Schema({
-    commentId: String,
-    repliesToThis: [String],
-    repliesToThat: String,
-    reportsToThat: String,
-    mentions: [String],
-    slug: String,
-    comment: [String],
-    username: String,
-    datetime: Date,
-    judgement: [{
-        username: String,
-        judgement: { type: String, enum: ['like', 'dislike'] }
-    }]
+    commentId: String, // do not use; use _id instead from mongo this is created by mongo by default and is unique
+    repliesToThis: { type: [String], default: [] },
+    repliesToThat: { type: String, default: null },
+    reportsToThat: { type: String, default: null }, // should be _id of the comment
+    mentions: { type: [String], default: [] },
+    slug: { type: String, required: true },
+    comment: { type: String, required: true },
+    username: { type: String, required: true },
+    datetime: { type: Date, required: true },
+    judgement: {
+        type: [{
+            username: {
+                type: String,
+                required: true
+            },
+            judgement: { type: String, enum: ['like', 'dislike'] }
+        }],
+        default: []
+    }
 });
 
 const CommentInfo = mongoose.model<CommentInfo>('Comment', commentInfoSchema);
+
+export default CommentInfo;
