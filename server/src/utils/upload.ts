1298f8e1b77390d07ee7ca46107ecd4fda7a8d38
dereckmezquita <dereck@mezquita.io>
2023-09-25 23:50:04 -0500

    Upload and save to client public for static serve.

--- server/src/utils/upload.ts
+++ server/src/utils/upload.ts
@@ -1,13 +1,15 @@
 import multer from 'multer';
 import path from 'path';
 
+import { ROOT_DIR_CLIENT_UPLOADS } from '@utils/constants';
+
 // way to save directly; we're not using this since doing all manipulation in memory
-const storage = multer.diskStorage({
-    destination: 'uploads/',
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
+// const storage = multer.diskStorage({
+//     destination: ROOT_DIR_CLIENT_UPLOADS,
+//     filename: (req, file, cb) => {
+//         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+//     }
+// });
 
 // Init upload
 const upload = multer({
