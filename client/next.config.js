f37b224e5487a8276f29c55675f0631494ceae84
dereckmezquita <dereck@mezquita.io>
2023-09-26 21:44:35 -0500

    test

--- client/next.config.js
+++ client/next.config.js
@@ -6,7 +6,6 @@ dotenv.config({ path: '../.env' });
 const nextConfig = {
     output: "export",
     reactStrictMode: true,
-
     compiler: {
         // https://stackoverflow.com/questions/67352231/why-all-styles-of-materialui-will-disappear-after-refresh-in-nextjs
         // https://nextjs.org/docs/advanced-features/compiler
