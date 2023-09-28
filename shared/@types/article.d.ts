98f3581791a0fe0defef8b7640a4acb3039aafa1
dereckmezquita <dereck@mezquita.io>
2023-09-28 10:50:55 -0500

    Name to match comment.

--- shared/@types/article.d.ts
+++ shared/@types/article.d.ts
@@ -3,6 +3,6 @@ export { };
 declare global {
     interface ArticleMetadata {
         slug: string; // client should send filename of the article
-        judgements: Map<string, 'like' | 'dislike'>;
+        judgement: Map<string, 'like' | 'dislike'>;
     }
 }
