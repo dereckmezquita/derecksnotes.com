1137aa88ec96455c52daa247cc1873a2ffe73e3e
dereckmezquita <dereck@mezquita.io>
2023-09-27 22:45:54 -0500

    Sanitise before save, rename, judgement is a map.

--- server/src/models/CommentInfo.ts
+++ server/src/models/CommentInfo.ts
@@ -1,6 +1,8 @@
 import { ObjectId } from "mongodb";
 import mongoose from "mongoose";
 
+import sanitizeHtml from 'sanitize-html';
+
 const commentSubSchema = new mongoose.Schema({
     content: {
         type: String,
@@ -24,7 +26,7 @@ const commentInfoSchema = new mongoose.Schema({
         ref: 'CommentInfo',
         default: null,
     },
-    reportsToThat: { // only added if the comment itself is a report of another comment
+    reportTarget: { // only added if the comment itself is a report of another comment
         type: ObjectId,
         ref: 'CommentInfo',
         default: null,
@@ -45,77 +47,110 @@ const commentInfoSchema = new mongoose.Schema({
         required: true,
         ref: 'User',
     },
-    judgement: { // likes/dislikes by users
-        type: [{
-            userId: {
-                type: ObjectId, // _id of the user
-                required: true,
-                ref: 'User',
-            },
-            judgement: {
-                type: String,
-                enum: ['like', 'dislike'],
-                required: true,
-            }
-        }],
-        default: []
+    judgement: { // likes/dislikes by users using Map structure
+        type: Map,
+        of: {
+            type: String,
+            enum: ['like', 'dislike'],
+            required: true
+        },
+        default: {}
     },
+    deleted: { type: Boolean, default: false }
 });
 
 // ---------------------------------------
 // pre save hooks
 // ---------------------------------------
-// trim the comment before saving
+// trim the comment before saving and sanitize it
 commentInfoSchema.pre('save', function (next) {
-    const comment = this;
+    const comment = this; // Now 'this' refers to the entire commentInfoSchema
     const lastCommentIndex = comment.content.length - 1;
 
     if (comment.content[lastCommentIndex]) {
-        comment.content[lastCommentIndex].content = comment.content[lastCommentIndex].content.trim();
+        // Trim the content
+        let trimmedContent = comment.content[lastCommentIndex].content.trim();
+
+        // Sanitize the content
+        let sanitizedContent = sanitizeHtml(trimmedContent, {
+            allowedTags: [
+                'b', 'i', 'u', 's', 'em', 'strong',
+                'ul', 'ol', 'li', 'a', 'blockquote',
+                'code', 'pre', 'br'
+            ],
+            allowedAttributes: {
+                'a': ['href'],
+                'blockquote': ['cite']
+            }
+        });
+
+        comment.content[lastCommentIndex].content = sanitizedContent;
     }
 
     next();
 });
 
+
 // ---------------------------------------
 // virtuals
 // ---------------------------------------
-commentInfoSchema.virtual('likesCount').get(function() {
-    return this.judgement.filter(j => j.judgement === 'like').length;
+commentInfoSchema.virtual('likesCount').get(function (this: CommentInfoDocument) {
+    let count = 0;
+    for (let [, judgement] of this.judgement) {
+        if (judgement === 'like') count++;
+    }
+    return count;
 });
 
-commentInfoSchema.virtual('dislikesCount').get(function() {
-    return this.judgement.filter(j => j.judgement === 'dislike').length;
+commentInfoSchema.virtual('dislikesCount').get(function (this: CommentInfoDocument) {
+    let count = 0;
+    for (let [, judgement] of this.judgement) {
+        if (judgement === 'dislike') count++;
+    }
+    return count;
 });
 
 // need to add virtuals to the interface
-commentInfoSchema.virtual('totalJudgement').get(function(this: CommentInfoDocument) {
+commentInfoSchema.virtual('totalJudgement').get(function (this: CommentInfoDocument) {
     return this.likesCount - this.dislikesCount;
 });
 
 // ---------------------------------------
 // methods
 // ---------------------------------------
+// ---- instance methods ----
 /*
 const comment = await CommentInfo.findById(someId);
 const latest = comment.latestContent();
 */
-commentInfoSchema.methods.latestContent = function() {
+commentInfoSchema.methods.latestContent = function () {
     return this.content[this.content.length - 1];
 };
 
+/*
+const comment = await CommentInfo.findById(commentId);
+comment.setJudgement(someUserId, 'like'); // or 'dislike'
+await comment.save();
+*/
+commentInfoSchema.methods.setJudgement = function (userId: string, judgement: 'like' | 'dislike') {
+    this.judgement.set(userId, judgement);
+}
+
+// ---- static methods ----
 /* const userComments = await CommentInfo.findByUser(someUserId); */
-commentInfoSchema.statics.findByUser = function(userId) {
+commentInfoSchema.statics.findByUser = function (userId) {
     return this.find({ userId });
 };
-
 
 // ---------------------------------------
 // interface for adding virtuals and methods
 interface CommentInfoDocument extends mongoose.Document {
+    judgement: Map<string, 'like' | 'dislike'>;
     likesCount: number;
     dislikesCount: number;
     totalJudgement: number;
+    latestContent: () => { content: string, createdAt: Date, updatedAt: Date };
+    setJudgement: (userId: string, judgement: 'like' | 'dislike') => void;
     // ... any other methods or virtuals you add
 }
 
