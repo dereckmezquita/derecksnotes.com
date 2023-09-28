0c3b9c96ce6545d5e0b2e12035ed93e4cd30f9f4
dereckmezquita <dereck@mezquita.io>
2023-09-28 18:18:34 -0500

    Api functions match end points.

---
+++ client/src/utils/api/interact/get_article_comments.ts
@@ -0,0 +1,22 @@
+import { API_PREFIX } from '@constants/config';
+
+const api_get_article_comments = async (slug: string, n: number, page: number = 1) => {
+    try {
+        const response = await fetch(`${API_PREFIX}/article_comments/${slug}?limit=${n}&page=${page}`, {
+            method: 'GET',
+            credentials: 'include',
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || 'Something went wrong fetching the comments!');
+        }
+
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export default api_get_article_comments;
---
+++ client/src/utils/api/interact/new_comment.ts
@@ -0,0 +1,25 @@
+import { API_PREFIX } from '@constants/config';
+
+const api_new_comment = async (content: string, slug: string, parentComment?: string) => {
+    try {
+        const response = await fetch(API_PREFIX + '/new_comment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ content, slug, parentComment }),
+            credentials: 'include',
+        });
+
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error(data.message || 'Something went wrong while posting the comment!');
+        }
+
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export default api_new_comment;
