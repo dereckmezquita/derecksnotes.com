d5e8fc6a900eec1dd38ef4c0ff5c6a5c2e2e2414
dereckmezquita <dereck@mezquita.io>
2023-09-28 21:22:02 -0500

    Added get comments route.

--- server/src/routes/interact/index.ts
+++ server/src/routes/interact/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import new_comment from './new_comment';
+import get_article_comments from './get_article_comments';
 
 const interactRoutes = Router();
 
 interactRoutes.use('/', new_comment);
+interactRoutes.use('/', get_article_comments);
 
 export default interactRoutes;
