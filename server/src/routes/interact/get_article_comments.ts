6feb4755942a809d6e853e37df33c759c631ecc5
dereckmezquita <dereck@mezquita.io>
2023-09-28 21:24:23 -0500

    Structured the type and added date filter.

--- server/src/routes/interact/get_article_comments.ts
+++ server/src/routes/interact/get_article_comments.ts
@@ -1,18 +1,25 @@
 import express from 'express';
-import CommentInfo from '@models/CommentInfo';  // Adjust the import path accordingly.
+import CommentInfo from '@models/CommentInfo';
 
 const get_article_comments = express.Router();
 
 get_article_comments.get('/get_article_comments/:slug', async (req, res) => {
     const { slug } = req.params;
-    const limit = Number(req.query.limit) || 10;  // default to 10 if limit isn't provided
+    const limit = Number(req.query.limit) || 10;
     const page = Number(req.query.page) || 1;
+    const startDate = req.query.startDate ? new Date(req.query.startDate as string) : undefined;
+    const endDate = req.query.endDate ? new Date(req.query.endDate as string) : undefined;
+
+    let dateFilter = {};
+    if (startDate && endDate) {
+        dateFilter = { "createdAt": { $gte: startDate, $lte: endDate } };
+    }
 
     try {
         const skip = (page - 1) * limit;
 
         // Fetch comments based on the slug and paginate using skip and limit
-        const comments = await CommentInfo.find({ slug })
+        const comments = await CommentInfo.find({ slug, ...dateFilter })
             .sort({ "createdAt": -1 })
             .skip(skip)
             .limit(limit);
@@ -20,11 +27,20 @@ get_article_comments.get('/get_article_comments/:slug', async (req, res) => {
         // Additionally, you can return the total number of comments for this article for frontend pagination purposes
         const total = await CommentInfo.countDocuments({ slug });
 
-        res.json({
-            comments,
+        const message: CommentInfoResponse = {
+            comments: comments.map((comment: any) => {
+                const commentObj = comment.toObject({
+                    virtuals: true,
+                    versionKey: false
+                });
+                delete commentObj.id; // Remove the 'id' field
+                return commentObj;
+            }),
             total,
             hasMore: total > page * limit
-        });
+        }
+
+        res.status(200).json(message);
     } catch (error) {
         res.status(500).json({ message: 'Internal Server Error' });
     }
