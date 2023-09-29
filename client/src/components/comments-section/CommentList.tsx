9531694653455e05d8a809826b189d5c5cc624d5
dereckmezquita <dereck@mezquita.io>
2023-09-29 10:18:42 -0500

    Loading comments, using new backend api.

--- client/src/components/comments-section/CommentList.tsx
+++ client/src/components/comments-section/CommentList.tsx
@@ -6,29 +6,26 @@ const ListContainer = styled.div`
     /* Add any desired styling for the container of the comments here */
 `;
 
-interface User {
-    id: string;
-    username: string;
-    profileImage: string;
-}
-
-interface UserComment {
-    id: string;
-    text: string;
-    author: User;
-    replies: UserComment[];
-}
-
 interface CommentListProps {
-    comments: UserComment[];
+    comments: CommentInfo[];
     currentUserId: string;
+    onReply?: (id: string) => void;
+    onEdit?: (id: string) => void;
+    onDelete?: (id: string) => void;
 }
 
-const CommentList: React.FC<CommentListProps> = ({ comments, currentUserId }) => {
+const CommentList: React.FC<CommentListProps> = ({ comments, currentUserId, onReply, onEdit, onDelete }) => {
     return (
         <ListContainer>
             {comments.map(comment => (
-                <Comment key={comment.id} comment={comment} userId={currentUserId} />
+                <Comment 
+                    key={comment._id} 
+                    comment={comment} 
+                    currentUserId={currentUserId} 
+                    onReply={onReply} 
+                    onEdit={onEdit} 
+                    onDelete={onDelete}
+                />
             ))}
         </ListContainer>
     );
