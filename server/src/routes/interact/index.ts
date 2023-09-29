075a30a8c0e37aa1f88eb4ddda1565ef66fad05c
dereckmezquita <dereck@mezquita.io>
2023-09-28 23:08:26 -0500

    Added new end points.

--- server/src/routes/interact/index.ts
+++ server/src/routes/interact/index.ts
@@ -2,10 +2,14 @@ import { Router } from 'express';
 
 import new_comment from './new_comment';
 import get_article_comments from './get_article_comments';
+import get_user_public_info from './get_user_public_info';
+import get_replies_comments from './get_replies_comments';
 
 const interactRoutes = Router();
 
 interactRoutes.use('/', new_comment);
 interactRoutes.use('/', get_article_comments);
+interactRoutes.use('/', get_user_public_info);
+interactRoutes.use('/', get_replies_comments);
 
 export default interactRoutes;
