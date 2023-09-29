0ed5f24230321ab63ba9336c062e7f08718d1eff
dereckmezquita <dereck@mezquita.io>
2023-09-28 23:04:25 -0500

    Testing public interface.

--- shared/@types/user.d.ts
+++ shared/@types/user.d.ts
@@ -1,7 +1,32 @@
 export { };
 
 declare global {
-    interface UserInfo {
+    // interface UserInfo {
+    //     name: {
+    //         first: string | null;
+    //         last: string | null;
+    //     };
+    //     profilePhotos: string[];
+    //     email: {
+    //         address: string; // must be unique
+    //         verified: boolean;
+    //     },
+    //     username: string; // must be unique
+    //     password?: string; // we don't send it back to user
+    //     metadata: {
+    //         geoLocations: GeoLocation[];
+    //         lastConnected: Date;
+    //         numberOfComments?: number;
+    //         commentsJudged?: {
+    //             commentId: string;
+    //             judgement: 'like' | 'dislike';
+    //         }[];
+    //     },
+    //     latestProfilePhoto?: string; // virtual
+    // }
+
+    // The public version of the user info
+    interface UserInfoPublicResponse {
         name: {
             first: string | null;
             last: string | null;
@@ -10,9 +35,14 @@ declare global {
         email: {
             address: string; // must be unique
             verified: boolean;
-        },
+        };
         username: string; // must be unique
-        password?: string; // we don't send it back to user
+        latestProfilePhoto?: string; // virtual
+    }
+
+    // Derived from the public version and includes additional properties for internal use
+    interface UserInfo extends UserInfoPublicResponse {
+        password?: string;
         metadata: {
             geoLocations: GeoLocation[];
             lastConnected: Date;
@@ -21,7 +51,6 @@ declare global {
                 commentId: string;
                 judgement: 'like' | 'dislike';
             }[];
-        },
-        latestProfilePhoto?: string; // virtual
+        };
     }
 }
