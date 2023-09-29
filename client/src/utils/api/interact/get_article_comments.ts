a20bb04062f29124b0ce327e8c7c0b826a9a3779
dereckmezquita <dereck@mezquita.io>
2023-09-28 21:25:30 -0500

    Date filter, fixed url.

--- client/src/utils/api/interact/get_article_comments.ts
+++ client/src/utils/api/interact/get_article_comments.ts
@@ -1,8 +1,14 @@
 import { API_PREFIX } from '@constants/config';
 
-const api_get_article_comments = async (slug: string, n: number, page: number = 1) => {
+const api_get_article_comments = async (slug: string, n?: number, page?: number, startDate?: string, endDate?: string) => {
     try {
-        const response = await fetch(`${API_PREFIX}/interact/article_comments/${slug}?limit=${n}&page=${page}`, {
+        const limitParam = n ? `limit=${n}` : '';
+        const pageParam = page ? `page=${page}` : '';
+        const startDateParam = startDate ? `startDate=${startDate}` : '';
+        const endDateParam = endDate ? `endDate=${endDate}` : '';
+        const queryString = [limitParam, pageParam, startDateParam, endDateParam].filter(p => p).join('&');
+
+        const response = await fetch(`${API_PREFIX}/interact/get_article_comments/${slug}?${queryString}`, {
             method: 'GET',
             credentials: 'include',
         });
