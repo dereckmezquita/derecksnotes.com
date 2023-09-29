d9cf01ab6c5eb347d61dc924373bc904d8e75194
dereckmezquita <dereck@mezquita.io>
2023-09-28 21:20:28 -0500

    Updated to match mongoose.

--- shared/@types/comment.d.ts
+++ shared/@types/comment.d.ts
@@ -1,22 +1,41 @@
 export { };
 
 declare global {
-    interface CommentInfo2 {
-        childComments: string[]; // _id of the child comments
-        parentComment: string; // _id of the parent comment
-        reportTarget?: string; // _id of a comment this one is reporting
+    interface CommentInfo {
+        _id?: string; // mongodb _id
+        childComments: string[];
+        parentComment: string | null;
+        reportTarget?: string | null;
         mentions?: string[]; // _id of the users mentioned in this comment
         slug: string; // slugs must be unique
         content: {
+            _id?: string; // mongodb _id
             comment: string;
             createdAt: Date; // added by mongoose
             updatedAt: Date;
         }[];
         userId: string; // _id of the user
-        // judgement is now a map
         judgement: {
             [userId: string]: 'like' | 'dislike';
         };
         deleted: boolean;
+        // virtuals
+        likesCount?: number;
+        dislikesCount?: number;
+        totalJudgements: number; // likesCount - dislikesCount
+        latestContent: {
+            comment: string;
+            _id?: string; // mongodb _id
+            createdAt: Date;
+            updatedAt: Date;
+        },
+        createdAt: Date;
+        updatedAt: Date;
+    }
+
+    interface CommentInfoResponse {
+        comments: CommentInfo[];
+        total: number;
+        hasMore: boolean;
     }
 }
