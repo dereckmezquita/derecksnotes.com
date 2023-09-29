7a7a44a6f85322825df9ce3ab28eea8a1551c5b4
dereckmezquita <dereck@mezquita.io>
2023-09-28 23:16:42 -0500

    Get comments by array of IDs.

--- server/src/routes/interact/index.ts
+++ server/src/routes/interact/index.ts
@@ -3,13 +3,13 @@ import { Router } from 'express';
 import new_comment from './new_comment';
 import get_article_comments from './get_article_comments';
 import get_user_public_info from './get_user_public_info';
-import get_replies_comments from './get_replies_comments';
+import get_comments_by_array_of_ids from './get_comments_by_array_of_ids';
 
 const interactRoutes = Router();
 
 interactRoutes.use('/', new_comment);
 interactRoutes.use('/', get_article_comments);
 interactRoutes.use('/', get_user_public_info);
-interactRoutes.use('/', get_replies_comments);
+interactRoutes.use('/', get_comments_by_array_of_ids);
 
 export default interactRoutes;
