0909e549be45caf2055e5e19ed632003f12e35eb
dereckmezquita <dereck@mezquita.io>
2023-09-29 21:44:21 -0500

    Added ability to reply, and limit max comment depth.

--- server/src/routes/interact/new_comment.ts
+++ server/src/routes/interact/new_comment.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
+import mongoose from 'mongoose';
+
 import CommentInfo from '@models/CommentInfo';
 import isAuthenticated from '@utils/middleware/isAuthenticated';
+
+import { MAX_COMMENT_DEPTH } from '@utils/constants';
 
 const new_comment = Router();
 
@@ -8,22 +12,34 @@ new_comment.use(isAuthenticated);
 
 new_comment.post('/new_comment', async (req, res) => {
     try {
-        const { comment, slug, parentComment } = req.body;
+        const { comment, slug, parentComment: parentId } = req.body as { comment: string, slug: string, parentComment?: string };
 
-        if (!comment || !slug) {
-            return res.status(400).json({ message: "Content and slug are required." });
-        }
+        console.log("Received: ", { comment, slug, parentId });
+
+        if (!comment || !slug) return res.status(400).json({ message: "Content and slug are required." });
 
         const newComment = new CommentInfo({
             content: [{ comment }],
             slug,
             userId: req.session.userId,
-            ...(parentComment && { parentComment })
+            ...(parentId && { parentComment: new mongoose.Types.ObjectId(parentId) })
         });
+
+        // now let's find the parent comment and add the id of this new comment to it
+        if (parentId) {
+            if (await getCommentDepth(parentId) >= MAX_COMMENT_DEPTH) return res.status(400).json({ message: "Reply depth limit reached." });
+
+            const parentComment = await CommentInfo.findOne<CommentInfo & mongoose.Document>({ _id: parentId });
+
+            if (!parentComment) return res.status(404).json({ message: "Parent comment not found." });
+
+            parentComment.childComments.push(newComment._id);
+
+            await parentComment.save();
+        }
 
         const savedComment = await newComment.save();
         res.status(201).json(savedComment);
-
     } catch (error) {
         console.error("Comment Post Error:", error);
         res.status(500).json({ message: "Unable to post the comment. Please try again." });
@@ -31,3 +47,11 @@ new_comment.post('/new_comment', async (req, res) => {
 });
 
 export default new_comment;
+
+async function getCommentDepth(commentId: string, depth: number = 0): Promise<number> {
+    const comment = await CommentInfo.findOne<CommentInfo & mongoose.Document>({ _id: commentId });
+    if (comment && comment.parentComment) {
+        return getCommentDepth(comment.parentComment.toString(), depth + 1);
+    }
+    return depth;
+}
