28641b640726d7275476a9dc134b566e36d08547
dereckmezquita <dereck@mezquita.io>
2023-09-29 21:48:26 -0500

    Limit reply button depth, pass slug for replies functionality.

--- client/src/components/comments-section/CommentList.tsx
+++ client/src/components/comments-section/CommentList.tsx
@@ -1,33 +1,29 @@
 import React from 'react';
-import styled from 'styled-components';
 import Comment from './Comment';
-
-const ListContainer = styled.div`
-    /* Add any desired styling for the container of the comments here */
-`;
 
 interface CommentListProps {
     comments: CommentInfo[];
-    currentUserId: string;
+    slug: string;
     onReply?: (id: string) => void;
     onEdit?: (id: string) => void;
     onDelete?: (id: string) => void;
 }
 
-const CommentList: React.FC<CommentListProps> = ({ comments, currentUserId, onReply, onEdit, onDelete }) => {
+const CommentList: React.FC<CommentListProps> = ({ comments, slug, onReply, onEdit, onDelete }) => {
     return (
-        <ListContainer>
+        <>
             {comments.map(comment => (
                 <Comment 
                     key={comment._id} 
                     comment={comment} 
-                    currentUserId={currentUserId} 
+                    slug={slug}
                     onReply={onReply} 
                     onEdit={onEdit} 
                     onDelete={onDelete}
+                    depth={0}
                 />
             ))}
-        </ListContainer>
+        </>
     );
 };
 
