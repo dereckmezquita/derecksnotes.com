d3d44e5257465202a3679732b2cf8c302b6f3007
dereckmezquita <dereck@mezquita.io>
2023-09-30 11:13:11 -0500

    End point responds with same structure as get comments end point for easily updating comments on front end.

--- server/src/routes/interact/new_comment.ts
+++ server/src/routes/interact/new_comment.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import mongoose from 'mongoose';
 
 import CommentInfo from '@models/CommentInfo';
+import User from '@models/User';
 import isAuthenticated from '@utils/middleware/isAuthenticated';
 
 import { MAX_COMMENT_DEPTH } from '@utils/constants';
@@ -13,8 +14,6 @@ new_comment.use(isAuthenticated);
 new_comment.post('/new_comment', async (req, res) => {
     try {
         const { comment, slug, parentComment: parentId } = req.body as { comment: string, slug: string, parentComment?: string };
-
-        console.log("Received: ", { comment, slug, parentId });
 
         if (!comment || !slug) return res.status(400).json({ message: "Content and slug are required." });
 
@@ -39,7 +38,20 @@ new_comment.post('/new_comment', async (req, res) => {
         }
 
         const savedComment = await newComment.save();
-        res.status(201).json(savedComment);
+
+        // getting username and profile photos
+        const user = await User.findById(req.session.userId) as { _id: string, username: string, latestProfilePhoto: string } & mongoose.Document;
+
+        const responseComment = {
+            ...savedComment.toObject({ virtuals: true, versionKey: false }),
+            username: user ? user.username : undefined,
+            latestProfilePhoto: user ? user.latestProfilePhoto : undefined,
+        };
+        delete responseComment.id;
+
+        res.status(201).json(responseComment);
+
+        console.log(responseComment);
     } catch (error) {
         console.error("Comment Post Error:", error);
         res.status(500).json({ message: "Unable to post the comment. Please try again." });
@@ -55,3 +67,73 @@ async function getCommentDepth(commentId: string, depth: number = 0): Promise<nu
     }
     return depth;
 }
+
+/*
+Server sent:
+{
+  childComments: [],
+  parentComment: null,
+  reportTarget: null,
+  mentions: [],
+  slug: 'chemistry_general-chemistry_a_acid',
+  content: [
+    {
+      comment: 'New comment received!',
+      _id: new ObjectId("651845ba4a09f09cce03e027"),
+      createdAt: 2023-09-30T15:58:50.518Z,
+      updatedAt: 2023-09-30T15:58:50.518Z
+    }
+  ],
+  userId: new ObjectId("65150eaf09acd7d63838949b"),
+  judgement: Map(0) {},
+  deleted: false,
+  _id: new ObjectId("651845ba4a09f09cce03e026"),
+  createdAt: 2023-09-30T15:58:50.518Z,
+  updatedAt: 2023-09-30T15:58:50.518Z,
+  likesCount: 0,
+  dislikesCount: 0,
+  totalJudgement: 0,
+  latestContent: {
+    comment: 'New comment received!',
+    _id: new ObjectId("651845ba4a09f09cce03e027"),
+    createdAt: 2023-09-30T15:58:50.518Z,
+    updatedAt: 2023-09-30T15:58:50.518Z
+  },
+  username: 'dereck',
+  latestProfilePhoto: 'optimised_dereck_2023-09-28-162359.jpg'
+}
+---
+Client received:
+{
+    "childComments": [],
+    "parentComment": null,
+    "reportTarget": null,
+    "mentions": [],
+    "slug": "chemistry_general-chemistry_a_acid",
+    "content": [
+        {
+            "comment": "New comment received!",
+            "_id": "651845ba4a09f09cce03e027",
+            "createdAt": "2023-09-30T15:58:50.518Z",
+            "updatedAt": "2023-09-30T15:58:50.518Z"
+        }
+    ],
+    "userId": "65150eaf09acd7d63838949b",
+    "judgement": {},
+    "deleted": false,
+    "_id": "651845ba4a09f09cce03e026",
+    "createdAt": "2023-09-30T15:58:50.518Z",
+    "updatedAt": "2023-09-30T15:58:50.518Z",
+    "likesCount": 0,
+    "dislikesCount": 0,
+    "totalJudgement": 0,
+    "latestContent": {
+        "comment": "New comment received!",
+        "_id": "651845ba4a09f09cce03e027",
+        "createdAt": "2023-09-30T15:58:50.518Z",
+        "updatedAt": "2023-09-30T15:58:50.518Z"
+    },
+    "username": "dereck",
+    "latestProfilePhoto": "optimised_dereck_2023-09-28-162359.jpg"
+}
+*/
