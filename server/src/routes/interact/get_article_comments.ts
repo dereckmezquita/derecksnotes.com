b6151249082b203ec1a7b5a99b5edab2c00ab2e4
dereckmezquita <dereck@mezquita.io>
2023-10-01 21:38:18 -0500

    Prototype for nested comment objects.

--- server/src/routes/interact/get_article_comments.ts
+++ server/src/routes/interact/get_article_comments.ts
@@ -1,63 +1,41 @@
 import express from 'express';
-import CommentInfo from '@models/CommentInfo';
-import User from '@models/User';
-import mongoose from 'mongoose';
+import Comment, { CommentDocument } from '@models/Comment';
 
 const get_article_comments = express.Router();
 
 get_article_comments.get('/get_article_comments/:slug', async (req, res) => {
     const { slug } = req.params;
-    const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 50;
     const page = Number(req.query.page) || 1;
+    const depth = Number(req.query.depth) || 1000; // max depth of child comments to populate
     const startDate = req.query.startDate ? new Date(req.query.startDate as string) : undefined;
     const endDate = req.query.endDate ? new Date(req.query.endDate as string) : undefined;
 
     let dateFilter = {};
     if (startDate && endDate) {
         dateFilter = { "createdAt": { $gte: startDate, $lte: endDate } };
+    } else if (startDate) {
+        dateFilter = { "createdAt": { $gte: startDate } };
+    } else if (endDate) {
+        dateFilter = { "createdAt": { $lte: endDate } };
     }
 
     try {
         const skip = (page - 1) * limit;
 
         // Fetch comments based on the slug and paginate using skip and limit
-        const comments = await CommentInfo.find<CommentInfo & mongoose.Document>({ slug, parentComment: null, ...dateFilter })
-            .sort({ "createdAt": -1 })
-            .skip(skip)
-            .limit(limit);
+        const comments = await getCommentsWithChildren(slug, limit, skip, dateFilter, depth);
 
-        // getting username and profile photos
-        const userIds: string[] = comments.map(comment => comment.userId);
-
-        // Fetch usernames and latest profile photos using $in operator
-        const users = await User.find({ '_id': { $in: userIds } }) as { _id: string, username: string, latestProfilePhoto: string }[] & mongoose.Document[];
-
-        const userMap = users.reduce((acc: any, user) => {
-            acc[user._id.toString()] = { username: user.username, latestProfilePhoto: user.latestProfilePhoto };
-            return acc;
-        }, {});
-
-        // Map through the comments and add username and latestProfilePhoto
-        const enrichedComments = comments.map(comment => {
-            const commentObj = comment.toObject({ virtuals: true, versionKey: false });
-            delete commentObj.id;
-
-            // Attach username and latestProfilePhoto
-            if (userMap[commentObj.userId]) {
-                commentObj.username = userMap[commentObj.userId].username;
-                commentObj.latestProfilePhoto = userMap[commentObj.userId].latestProfilePhoto;
-            }
-
-            return commentObj;
-        });
-
-        // Additionally, return the total number of comments for this article for frontend pagination purposes
-        const total = await CommentInfo.countDocuments({ slug, parentComment: null });
+        console.log(
+            '\x1b[31m%s\x1b[0m------------------ Final result object ------------------\n',
+            JSON.stringify(comments),
+            '\x1b[0m'
+        )
 
         const message: CommentInfoResponse = {
-            comments: enrichedComments,
-            total,
-            hasMore: total > page * limit
+            comments: comments as any,
+            total: totalCount,
+            hasMore: false, // TODO: implement a pagination system for getting more top level comments
         }
 
         res.status(200).json(message);
@@ -69,43 +47,61 @@ get_article_comments.get('/get_article_comments/:slug', async (req, res) => {
 
 export default get_article_comments;
 
-/*
-{
-    "comments": [
-        {
-            "_id": "6516334978a1c4f861ecc94a",
-            "childComments": [],
-            "parentComment": null,
-            "reportTarget": null,
-            "mentions": [],
-            "slug": "chemistry_general-chemistry_a_acid",
-            "content": [
-                {
-                    "comment": "Some first comment!",
-                    "_id": "6516334978a1c4f861ecc94b",
-                    "createdAt": "2023-09-29T02:15:37.991Z",
-                    "updatedAt": "2023-09-29T02:15:37.991Z"
-                }
-            ],
-            "userId": "65150eaf09acd7d63838949b",
-            "judgement": {},
-            "deleted": false,
-            "createdAt": "2023-09-29T02:15:37.991Z",
-            "updatedAt": "2023-09-29T02:15:37.991Z",
-            "likesCount": 0,
-            "dislikesCount": 0,
-            "totalJudgement": 0,
-            "latestContent": {
-                "comment": "Some first comment!",
-                "_id": "6516334978a1c4f861ecc94b",
-                "createdAt": "2023-09-29T02:15:37.991Z",
-                "updatedAt": "2023-09-29T02:15:37.991Z"
-            },
-            "username": "dereck",
-            "latestProfilePhoto": "optimised_dereck_2023-09-28-162359.jpg"
+let totalCount: number = 0; // count total number of comments including child comments
+
+// use totalCount to count total number of comments including child comments
+// this logic happens in the populateChildComments function in the for loop that calls populateChildComments
+async function populateChildComments(comment: CommentDocument, maxDepth: number): Promise<CommentDocument> {
+    if (comment.childComments && comment.childComments.length > 0) {
+        comment = await Comment.populate(comment, {
+            path: 'childComments',
+            model: 'Comment'
+        });
+
+        console.log(
+            '\x1b[32m%s\x1b[0m------------------ ChildComment populated ------------------\n',
+            JSON.stringify(comment),
+            '\x1b[0m'
+        )
+
+        // Deeply populate the child comments
+        for (let i = 0; i < comment.childComments.length; i++) {
+            comment.childComments[i] = await populateChildComments(comment.childComments[i] as CommentDocument, maxDepth - 1);
+
+            // increment totalCount for each child comment
+            totalCount++;
         }
-    ],
-    "total": 1,
-    "hasMore": false
+    }
+    // yellow console.log
+    console.log(
+        '\x1b[33m%s\x1b[0m------------------ before Child.toObject ------------------\n',
+        JSON.stringify(comment),
+        '\x1b[0m'
+    )
+    return comment.toObject({ virtuals: true });
 }
-*/
+
+async function getCommentsWithChildren(
+    slug: string,
+    limit: number,
+    skip: number,
+    dateFilter: {},
+    maxDepth: number
+): Promise<CommentDocument[]> {
+    let topLevelComments = await Comment.find<CommentDocument>({
+        slug: slug,
+        parentComment: null,
+        ...dateFilter
+    })
+        .skip(skip) // skip for getting next page
+        .limit(limit); // limit for number of comments per page
+
+    totalCount = topLevelComments.length;
+
+    for (let i = 0; i < topLevelComments.length; i++) {
+        topLevelComments[i] = await populateChildComments(topLevelComments[i], maxDepth);
+    }
+
+    // already populated and converted to object
+    return topLevelComments;
+}
