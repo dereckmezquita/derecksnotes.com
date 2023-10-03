1ba45b0551edbd067010ab76d4501fb435ca4b01
dereckmezquita <dereck@mezquita.io>
2023-10-02 22:05:08 -0500

    Deleted old commented interface.

--- shared/@types/user.d.ts
+++ shared/@types/user.d.ts
@@ -1,30 +1,6 @@
 export { };
 
 declare global {
-    // interface UserInfo {
-    //     name: {
-    //         first: string | null;
-    //         last: string | null;
-    //     };
-    //     profilePhotos: string[];
-    //     email: {
-    //         address: string; // must be unique
-    //         verified: boolean;
-    //     },
-    //     username: string; // must be unique
-    //     password?: string; // we don't send it back to user
-    //     metadata: {
-    //         geoLocations: GeoLocation[];
-    //         lastConnected: Date;
-    //         numberOfComments?: number;
-    //         commentsJudged?: {
-    //             commentId: string;
-    //             judgement: 'like' | 'dislike';
-    //         }[];
-    //     },
-    //     latestProfilePhoto?: string; // virtual
-    // }
-
     // The public version of the user info
     interface UserInfoPublicResponse {
         profilePhotos: string[];
