22bdfff95309693591e7358adf48dc45d6449562
dereckmezquita <dereck@mezquita.io>
2023-10-02 23:18:21 -0500

    Reworked new_comment to use populate.

--- server/src/routes/interact/index.ts
+++ server/src/routes/interact/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import new_comment from './new_comment';
 import get_article_comments from './get_article_comments';
 import get_user_public_info from './get_user_public_info';
-import get_comments_by_array_of_ids from './get_comments_by_array_of_ids';
+import get_comments_threads_by_id from './get_comments_threads_by_id';
 import delete_comments from './delete_comments';
 
 const interactRoutes = Router();
@@ -11,7 +11,7 @@ const interactRoutes = Router();
 interactRoutes.use('/', new_comment);
 interactRoutes.use('/', get_article_comments);
 interactRoutes.use('/', get_user_public_info);
-interactRoutes.use('/', get_comments_by_array_of_ids);
+interactRoutes.use('/', get_comments_threads_by_id);
 interactRoutes.use('/', delete_comments);
 
 export default interactRoutes;
