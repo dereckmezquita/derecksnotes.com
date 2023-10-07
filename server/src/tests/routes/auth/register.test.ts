c4383d04ee09c172d95f3882874a75bd8d6fee1d
dereckmezquita <dereck@mezquita.io>
2023-10-07 11:55:01 -0500

    Draft.

--- server/src/tests/routes/auth/register.test.ts
+++ server/src/tests/routes/auth/register.test.ts
@@ -1,46 +1,34 @@
 import request from 'supertest';
 import { SetUp, app, redisClient } from '../../../index';
+import { InMemoryDBConnector } from '@utils/DatabaseConnector';
 
 import { API_PREFIX } from '@utils/constants';
 
-// jest.mock('redis', () => {
-//     const redisMock = require('redis-mock');
-//     const originalCreateClient = redisMock.createClient;
+jest.mock('@utils/geoLocate', () => ({
+    __esModule: true,
+    default: jest.fn().mockResolvedValue({
+        ip: '127.0.0.1',
+        country: 'United States',
+        countryCode: 'US',
+        flag: '🇺🇸',
+        regionName: 'California',
+        city: 'San Francisco',
+        isp: 'ISP_NAME',
+        org: 'ORG_NAME'
+    }),
+}));
 
-//     redisMock.createClient = () => ({
-//         ...originalCreateClient(),
-//         connect: jest.fn().mockResolvedValue(undefined),
-//         set: jest.fn().mockResolvedValue(undefined),
-//         get: jest.fn().mockResolvedValue(undefined),
-//     });
-
-//     return redisMock;
-// });
-
-// jest.mock('@utils/geoLocate', () => ({
-//     __esModule: true,
-//     default: jest.fn().mockResolvedValue({
-//         ip: '127.0.0.1',
-//         country: 'United States',
-//         countryCode: 'US',
-//         flag: '🇺🇸',
-//         regionName: 'California',
-//         city: 'San Francisco',
-//         isp: 'ISP_NAME',
-//         org: 'ORG_NAME'
-//     }),
-// }));
-
-describe('POST /auth/register', () => {
+describe('Get User Public Info Route', () => {
     beforeAll(async () => {
-        await SetUp();
+        const db = new InMemoryDBConnector();
+        await SetUp(db);
     });
 
     afterAll(async () => {
         // await redisClient.disconnect();
     });
-    
-    it('should successfully register a new user', async () => {
+
+    it('should return user public info', async () => {
         const payload = {
             email: 'test@example.com',
             username: 'testuser',
@@ -49,11 +37,12 @@ describe('POST /auth/register', () => {
 
         const res = await request(app)
             .post(API_PREFIX + '/auth/register')
+            .set('Content-Type', 'application/json')
             .send(payload);
 
         expect(res.status).toBe(201);
         expect(res.body.message).toBe('User registered and logged in successfully');
-    }, 5000);
+    });
 
     it('should fail if the user already exists', async () => {
         const payload = {
@@ -64,15 +53,15 @@ describe('POST /auth/register', () => {
 
         // First, register the user
         await request(app)
-            .post(`${API_PREFIX}/auth/register`)
+            .post(API_PREFIX + '/auth/register')
             .send(payload);
 
         // Try to register the same user again
         const res = await request(app)
-            .post(`${API_PREFIX}/auth/register`)
+            .post(API_PREFIX + '/auth/register')
             .send(payload);
 
         expect(res.status).toBe(400);
         expect(res.body.message).toBe('User already exists.');
-    }, 5000);
+    });
 });
