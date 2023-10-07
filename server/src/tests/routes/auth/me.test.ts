b8f2c740e263311817325d0ab1edc3a3f37dc7d6
dereckmezquita <dereck@mezquita.io>
2023-10-07 12:54:40 -0500

    Could not use module.

--- server/src/tests/routes/auth/me.test.ts
+++ server/src/tests/routes/auth/me.test.ts
@@ -6,7 +6,19 @@ import User, { UserDocument } from '@models/User';
 
 import { userMock } from '../../mocks/user';
 
-import "../../mocks/geoLocate";
+jest.mock('@utils/geoLocate', () => ({
+    __esModule: true,
+    default: jest.fn().mockResolvedValue({
+        ip: '127.0.0.1',
+        country: 'United States',
+        countryCode: 'US',
+        flag: '🇺🇸',
+        regionName: 'California',
+        city: 'San Francisco',
+        isp: 'ISP_NAME',
+        org: 'ORG_NAME'
+    }),
+}));
 
 describe('GET /me Endpoint', () => {
     let dbConnector: InMemoryDBConnector;
