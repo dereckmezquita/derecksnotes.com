59440879c31549dfc9516d8efae353e24619ef14
dereckmezquita <dereck@mezquita.io>
2023-10-07 10:08:07 -0500

    General date function.

--- client/src/constants/dates.ts
+++ client/src/constants/dates.ts
@@ -13,3 +13,16 @@ export enum DAYS_OF_WEEK {
 export enum MONTHS_OF_YEAR {
     JAN = 0, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
 }
+
+export function FORMAT_DATE_YYYY_MM_DD_HHMMSS(d: Date | string): string {
+    const date = d instanceof Date ? d : new Date(d);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    const second = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day}, ${hour}:${minute}:${second}`;
+};
