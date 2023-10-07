c0d4e8b83d315973a02145e6742889af75325314
dereckmezquita <dereck@mezquita.io>
2023-10-07 10:20:00 -0500

    Draft.

--- server/src/routes/interact/new_comment.ts
+++ server/src/routes/interact/new_comment.ts
@@ -2,20 +2,20 @@ import { Router } from 'express';
 import mongoose from 'mongoose';
 
 import Comment, { CommentDocument } from '@models/Comment';
-import User, { UserDocument } from '@models/User';
+import User from '@models/User';
 import isAuthenticated from '@utils/middleware/isAuthenticated';
 
 import { MAX_COMMENT_DEPTH } from '@utils/constants';
 
 const new_comment = Router();
 
-new_comment.use(isAuthenticated);
-
-new_comment.post('/new_comment', async (req, res) => {
+new_comment.post('/new_comment', isAuthenticated, async (req, res) => {
     try {
         const { comment, slug, parentComment: parentId } = req.body as { comment: string, slug: string, parentComment?: string };
 
-        if (!comment || !slug) return res.status(400).json({ message: "Content and slug are required." });
+        if (!comment || !slug) {
+            return res.status(400).json({ message: "Content and slug are required." });
+        }
 
         const newComment: CommentDocument = new Comment({
             content: [{ comment }],
@@ -26,11 +26,15 @@ new_comment.post('/new_comment', async (req, res) => {
 
         // now let's find the parent comment and add the id of this new comment to it
         if (parentId) {
-            if (await getCommentDepth(parentId) >= MAX_COMMENT_DEPTH) return res.status(400).json({ message: "Reply depth limit reached." });
+            if (await getCommentDepth(parentId) >= MAX_COMMENT_DEPTH) {
+                return res.status(400).json({ message: "Reply depth limit reached." });
+            }
 
             const parentComment = await Comment.findOne<CommentDocument>({ _id: parentId });
 
-            if (!parentComment) return res.status(404).json({ message: "Parent comment not found." });
+            if (!parentComment) {
+                return res.status(404).json({ message: "Parent comment not found." });
+            }
 
             parentComment.childComments.push(newComment._id);
 
@@ -39,15 +43,15 @@ new_comment.post('/new_comment', async (req, res) => {
 
         await newComment.save();
 
-        await Comment.populate(newComment, {
-            path: 'userId',
+        const popObj = (await Comment.populate(newComment, {
+            path: 'user',
             select: 'username profilePhotos latestProfilePhoto',
             model: User
-        })
+        })).toObject({ virtuals: true })
 
-        res.status(201).json(newComment.toObject({ virtuals: true }));
+        // console.log(JSON.stringify(popObj));
 
-        console.log(newComment);
+        res.status(201).json(popObj);
     } catch (error) {
         console.error("Comment Post Error:", error);
         res.status(500).json({ message: "Unable to post the comment. Please try again." });
@@ -65,4 +69,50 @@ async function getCommentDepth(commentId: string, depth: number = 0): Promise<nu
 }
 
 /*
+Server sent:
+{
+  childComments: [],
+  parentComment: null,
+  reportTarget: null,
+  mentions: [],
+  slug: 'chemistry_general-chemistry_a_acid',
+  content: [
+    {
+      comment: 'mmm',
+      _id: new ObjectId("651f78994c0a204990c13782"),
+      createdAt: 2023-10-06T03:01:45.483Z,
+      updatedAt: 2023-10-06T03:01:45.483Z
+    }
+  ],
+  userId: new ObjectId("65150eaf09acd7d63838949b"),
+  judgement: Map(0) {},
+  deleted: false,
+  _id: new ObjectId("651f78994c0a204990c13781"),
+  createdAt: 2023-10-06T03:01:45.483Z,
+  updatedAt: 2023-10-06T03:01:45.483Z,
+  __v: 0,
+  likesCount: 0,
+  dislikesCount: 0,
+  totalJudgement: 0,
+  latestContent: {
+    comment: 'mmm',
+    _id: new ObjectId("651f78994c0a204990c13782"),
+    createdAt: 2023-10-06T03:01:45.483Z,
+    updatedAt: 2023-10-06T03:01:45.483Z
+  },
+  user: {
+    _id: new ObjectId("65150eaf09acd7d63838949b"),
+    profilePhotos: [
+      'optimised_dereck_2023-09-28-162359.jpg',
+      'optimised_dereck_2023-10-04-024459.png'
+    ],
+    username: 'dereck',
+    latestProfilePhoto: 'optimised_dereck_2023-10-04-024459.png',
+    id: '65150eaf09acd7d63838949b'
+  },
+  latestProfilePhoto: 'optimised_dereck_2023-10-04-024459.png',
+  id: '651f78994c0a204990c13781'
+}
+Client received:
+{"childComments":[],"parentComment":null,"reportTarget":null,"mentions":[],"slug":"chemistry_general-chemistry_a_acid","content":[{"comment":"mmm","_id":"651f78994c0a204990c13782","createdAt":"2023-10-06T03:01:45.483Z","updatedAt":"2023-10-06T03:01:45.483Z"}],"userId":"65150eaf09acd7d63838949b","judgement":{},"deleted":false,"_id":"651f78994c0a204990c13781","createdAt":"2023-10-06T03:01:45.483Z","updatedAt":"2023-10-06T03:01:45.483Z","__v":0,"likesCount":0,"dislikesCount":0,"totalJudgement":0,"latestContent":{"comment":"mmm","_id":"651f78994c0a204990c13782","createdAt":"2023-10-06T03:01:45.483Z","updatedAt":"2023-10-06T03:01:45.483Z"},"user":{"_id":"65150eaf09acd7d63838949b","profilePhotos":["optimised_dereck_2023-09-28-162359.jpg","optimised_dereck_2023-10-04-024459.png"],"username":"dereck","latestProfilePhoto":"optimised_dereck_2023-10-04-024459.png","id":"65150eaf09acd7d63838949b"},"latestProfilePhoto":"optimised_dereck_2023-10-04-024459.png","id":"651f78994c0a204990c13781"}
 */
