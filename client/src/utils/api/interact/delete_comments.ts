e13b917580fdfcf9d3bbc6c955d5cff42ced0971
dereckmezquita <dereck@mezquita.io>
2023-10-07 10:04:33 -0500

    Comment with expected result.

--- client/src/utils/api/interact/delete_comments.ts
+++ client/src/utils/api/interact/delete_comments.ts
@@ -17,6 +17,8 @@ const api_delete_comments = async (commentIds: string[]) => {
             throw new Error(data.message || 'Something went wrong while deleting the comment!');
         }
 
+        // console.log(JSON.stringify(data));
+
         return data;
     } catch (error) {
         throw error;
@@ -24,3 +26,8 @@ const api_delete_comments = async (commentIds: string[]) => {
 }
 
 export default api_delete_comments;
+
+/*
+Client received:
+{"comments":[{"_id":"651f76e2f0cf6928b3c2ff4b","childComments":[],"parentComment":null,"reportTarget":null,"mentions":[],"slug":"chemistry_general-chemistry_a_acid","content":[{"comment":"lll","_id":"651f76e2f0cf6928b3c2ff4c","createdAt":"2023-10-06T02:54:26.879Z","updatedAt":"2023-10-06T02:54:26.879Z"},{"comment":"[deleted]","_id":"651f8eac4c0a204990c13981","createdAt":"2023-10-06T04:35:56.269Z","updatedAt":"2023-10-06T04:35:56.269Z"}],"userId":{"_id":"65150eaf09acd7d63838949b","profilePhotos":["optimised_dereck_2023-09-28-162359.jpg","optimised_dereck_2023-10-04-024459.png"],"username":"dereck","latestProfilePhoto":"optimised_dereck_2023-10-04-024459.png","id":"65150eaf09acd7d63838949b"},"judgement":{},"deleted":true,"createdAt":"2023-10-06T02:54:26.879Z","updatedAt":"2023-10-06T04:35:56.270Z","__v":1,"likesCount":0,"dislikesCount":0,"totalJudgement":0,"latestContent":{"comment":"[deleted]","_id":"651f8eac4c0a204990c13981","createdAt":"2023-10-06T04:35:56.269Z","updatedAt":"2023-10-06T04:35:56.269Z"},"latestProfilePhoto":null,"id":"651f76e2f0cf6928b3c2ff4b"}],"hasMore":false}
+*/
