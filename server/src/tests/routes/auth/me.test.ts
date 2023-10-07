baf882ee860994eff0143b0f76c180ecdecdf25d
dereckmezquita <dereck@mezquita.io>
2023-10-07 12:43:20 -0500

    Modularise geoLocate.

--- server/src/tests/routes/auth/me.test.ts
+++ server/src/tests/routes/auth/me.test.ts
@@ -6,19 +6,7 @@ import User, { UserDocument } from '@models/User';
 
 import { userMock } from '../../mocks/user';
 
-jest.mock('@utils/geoLocate', () => ({
-    __esModule: true,
-    default: jest.fn().mockResolvedValue({
-        ip: '127.0.0.1',
-        country: 'United States',
-        countryCode: 'US',
-        flag: '🇺🇸',
-        regionName: 'California',
-        city: 'San Francisco',
-        isp: 'ISP_NAME',
-        org: 'ORG_NAME'
-    }),
-}));
+import "../../mocks/geoLocate";
 
 describe('GET /me Endpoint', () => {
     let dbConnector: InMemoryDBConnector;
