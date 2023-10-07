ccf339c75fddc6c4f41bdad2dda204039b80f41d
dereckmezquita <dereck@mezquita.io>
2023-10-07 12:10:48 -0500

    Moved to use in memory db.

---
+++ server/src/tests/mocks/user.ts
@@ -0,0 +1,11 @@
+export const userMock: UserDTO = {
+    name: { first: 'Dereck', last: 'Mezquita' },
+    profilePhotos: ['optimised_dereck_2023-09-28-162359.jpg'],
+    email: { address: 'test@example.com', verified: false },
+    username: 'dereck',
+    password: 'testpassword',
+    metadata: {
+        geolocations: [],
+        lastConnected: new Date(),
+    }
+};
