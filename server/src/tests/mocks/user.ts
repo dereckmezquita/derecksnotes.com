e8b650bf5fbc4f65a7663b6f9ca335fcfdab49be
dereckmezquita <dereck@mezquita.io>
2023-10-07 12:19:30 -0500

    Draft.

--- server/src/tests/mocks/user.ts
+++ server/src/tests/mocks/user.ts
@@ -1,7 +1,7 @@
 export const userMock: UserDTO = {
     name: { first: 'Dereck', last: 'Mezquita' },
     profilePhotos: ['optimised_dereck_2023-09-28-162359.jpg'],
-    email: { address: 'test@example.com', verified: false },
+    email: { address: 'dereck@example.com', verified: false },
     username: 'dereck',
     password: 'testpassword',
     metadata: {
