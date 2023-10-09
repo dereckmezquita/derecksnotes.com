35e34a32f2b7305014411b3df3fd90807c8f5e12
dereckmezquita <dereck@mezquita.io>
2023-10-09 13:22:34 -0500

    Mock second test user.

--- server/src/tests/mocks/user.ts
+++ server/src/tests/mocks/user.ts
@@ -9,3 +9,15 @@ export const userMock: UserDTO = {
         lastConnected: new Date(),
     }
 };
+
+export const userMock2: UserDTO = {
+    name: { first: 'Test', last: 'User' },
+    profilePhotos: ['optimised_test_user_2023-09-28-162359.jpg'],
+    email: { address: 'test@test.com', verified: false },
+    username: 'test_user',
+    password: 'testpassword',
+    metadata: {
+        geolocations: [],
+        lastConnected: new Date(),
+    }
+}
