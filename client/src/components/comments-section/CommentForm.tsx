6d4c6e6e281e18a8c0cb7a265647378162f58b83
dereckmezquita <dereck@mezquita.io>
2023-10-12 11:51:16 -0500

    Working on implementing flat list of comments.

--- client/src/components/comments-section/CommentForm.tsx
+++ client/src/components/comments-section/CommentForm.tsx
@@ -1,20 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+// --------------------------------------
 import styled from 'styled-components';
 import { theme } from '@styles/theme';
-
+// --------------------------------------
 import api_new_comment from '@utils/api/interact/new_comment';
-
+// --------------------------------------
 import Button from '@components/atomic/Button';
-
 import IndicateLoading from '@components/atomic/IndicateLoading';
-import IndicateError from '@components/atomic/IndicateError';
-import IndicateSuccess from '@components/atomic/IndicateSuccess';
 
 const Form = styled.form`
     background-color: ${theme.container.background.colour.primary()};
-    border-top: 1px dashed ${theme.container.border.colour.primary()};
-    margin-top: 20px;
-    margin-bottom: 10px;
     padding-left: 5px;
     padding-right: 5px;
     border-radius: 5px;
@@ -24,7 +19,7 @@ const Input = styled.textarea`
     width: 100%;
     padding: 10px;
     font-family: ${theme.text.font.arial};
-    border: 1px solid ${theme.container.border.colour.primary()};
+    border: 2px solid ${theme.container.border.colour.primary()};
     border-radius: 5px;
     margin-top: 10px;
     margin-bottom: 10px;
@@ -35,81 +30,46 @@ const Input = styled.textarea`
 interface CommentFormProps {
     slug: string;
     parentComment?: string;
-    onSubmit: (comment: CommentPopUserDTO) => void;
+    onSubmit: (comment: CommentPopUserDTO) => void; // callback sends new comment to parent component
 }
 
 const CommentForm: React.FC<CommentFormProps> = ({ slug, parentComment, onSubmit }) => {
-    const [comment, setComment] = useState('');
+    const [commentInput, setCommentInput] = useState('');
     const [isLoading, setIsLoading] = useState(false);
-    const [message, setMessage] = useState<string | null>(null);
-    const [shouldRenderMessage, setShouldRenderMessage] = useState(true);
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        setIsLoading(true);  // Set loading state
-        setMessage(null); // Clear any existing messages
-        if (comment.trim()) {
+        setIsLoading(true);
+        if (commentInput.trim()) {
             try {
-                const response = await api_new_comment(comment, slug, parentComment);
-                setIsLoading(false); // Reset loading state
-                if (response) {
-                    onSubmit(response);
-                    setComment('');
-                    setMessage('Comment posted successfully'); // Set success message
-                } else {
-                    setMessage('Failed to post comment'); // Handle non-successful response here
+                const res = await api_new_comment(commentInput, slug, parentComment);
+                setIsLoading(false);
+                if (res) {
+                    onSubmit(res);
+                    setCommentInput('');
                 }
-            } catch (error: any) {
-                setIsLoading(false); // Reset loading state
-                setMessage('An error occurred while posting the comment'); // Set error message
-                console.error("Error submitting the comment:", error.message);
+            } catch (error) {
+                setIsLoading(false);
             }
         } else {
-            setIsLoading(false); // Reset loading state
-            setMessage('Comment cannot be empty'); // Set message for empty comment
+            setIsLoading(false);
         }
-    };
-
-    // Function to clear the message and initiate the "animate out" process
-    const clearMessage = () => {
-        setShouldRenderMessage(false);
-    };
-
-    useEffect(() => {
-        if (message) {
-            setShouldRenderMessage(true);
-            // Optionally, clear the message after a timeout, for example, 5 seconds
-            const timerId = setTimeout(() => {
-                clearMessage();
-            }, 1000);
-
-            // Cleanup
-            return () => {
-                clearTimeout(timerId);
-            };
-        }
-    }, [message]);
+    }
 
     return (
         <Form onSubmit={handleSubmit}>
-            {parentComment ? null : <h2 style={{ marginTop: '20px' }}>Comments</h2>}
             <Input
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                placeholder="Add a comment..."
+                value={commentInput}
+                onChange={e => setCommentInput(e.target.value)}
+                placeholder='Write a comment...'
                 rows={1}
             />
-            <Button type="submit" disabled={isLoading}>Post</Button>
-            {isLoading &&
-                <IndicateLoading />
-            }
-            {message && shouldRenderMessage && (
-                message === 'Comment posted successfully' ?
-                    <IndicateSuccess message={message} shouldRender={shouldRenderMessage} /> :
-                    <IndicateError message={message} shouldRender={shouldRenderMessage} />
-            )}
+            <Button type='submit' disabled={isLoading}>
+                Post
+            </Button>
+            {isLoading && <IndicateLoading />}
         </Form>
-    );
-};
+    )
+}
 
 export default CommentForm;
--- client/src/components/comments-section/CommentSection.tsx
+++ client/src/components/comments-section/CommentSection.tsx
@@ -1,122 +1,66 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import CommentForm from './CommentForm';
-import CommentList from './CommentList';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
-import api_get_article_comments from '@utils/api/interact/get_article_comments';
-import api_delete_comments from '@utils/api/interact/delete_comments';
+import { theme } from "@styles/theme";
 
-interface CommentsSectionProps {
+// --------------------------------------
+import CommentForm from "./CommentForm";
+
+// --------------------------------------
+import api_get_article_comments from "@utils/api/interact/get_article_comments";
+
+// --------------------------------------
+import IndicateLoading from "@components/atomic/IndicateLoading";
+
+const CommentSectionContainer = styled.div`
+    background-color: ${theme.container.background.colour.primary()};
+    border-top: 1px dashed ${theme.container.border.colour.primary()};
+`;
+
+interface CommentSectionProps {
     slug: string;
     allowComments?: boolean;
 }
 
-const CommentSection: React.FC<CommentsSectionProps> = ({ slug, allowComments }) => {
-    // ----------------------------------------------------------------
-    // load initial comments using slug from server
-    const [comments, setComments] = useState<CommentsBySlugDTO>();
+const CommentSection = ({ slug, allowComments }: CommentSectionProps) => {
+    const [comments, setComments] = useState<CommentPopUserDTO[]>([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchComments = async () => {
             try {
-                setComments(await api_get_article_comments(slug));
-            } catch (error) {
-                console.error('Failed to fetch comments:', error);
+                const res: CommentsBySlugDTO = await api_get_article_comments(slug);
+                setComments(res.comments);
+                setLoading(false);
+            } catch (error: any) {
+                setLoading(false);
             }
         }
-
         fetchComments();
-    }, [slug]);
+    }, []);
 
-    const updateCommentReplies = (comments: CommentPopUserDTO[], newReply: CommentPopUserDTO): CommentPopUserDTO[] => {
-        return comments.map(comment => {
-            if (comment._id === newReply.parentComment) {
-                comment.childComments = [...(comment.childComments as CommentPopUserDTO[]), newReply];
-                return comment;
-            } else {
-                comment.childComments = updateCommentReplies(comment.childComments as CommentPopUserDTO[], newReply);
-                return comment;
-            }
-        });
-    }
+    if (loading) return <IndicateLoading />;
 
     // -----------------
     // new comment update dom using callback from CommentForm
-    const handleNewComment = useCallback((newComment: CommentPopUserDTO) => {
-        if (newComment.parentComment) {
-            setComments(prevState => {
-                if (prevState && prevState.comments) {
-                    return {
-                        ...prevState,
-                        comments: updateCommentReplies(prevState.comments, newComment)
-                    };
-                }
-                return prevState;
-            });
-        } else {
-            // The existing logic to handle new top-level comments
-            setComments(prevState => {
-                if (prevState) {
-                    return {
-                        ...prevState,
-                        comments: [newComment, ...prevState.comments]
-                    };
-                } else {
-                    return {
-                        comments: [newComment],
-                        hasMore: false
-                    };
-                }
-            });
-        }
-    }, []);
-
-    const handleDeleteComment = useCallback(async (commentId: string) => {
-        try {
-            const deletedCommentRes: CommentsBySlugDTO = await api_delete_comments([commentId]);
-            const deletedComment: CommentPopUserDTO = deletedCommentRes.comments[0];
-
-            setComments(prevState => {
-                if (!prevState) return undefined;
-
-                const findAndReplaceComment = (comments: CommentPopUserDTO[]): CommentPopUserDTO[] => {
-                    return comments.map(comment => {
-                        if (comment._id === deletedComment._id) {
-                            return deletedComment;
-                        }
-                        if (comment.childComments && comment.childComments.length) {
-                            return {
-                                ...comment,
-                                childComments: findAndReplaceComment(comment.childComments as CommentPopUserDTO[])
-                            };
-                        }
-                        return comment;
-                    });
-                };
-
-                return {
-                    ...prevState,
-                    comments: findAndReplaceComment(prevState.comments)
-                };
-            });
-        } catch (error) {
-            console.error('Failed to delete comment:', error);
-        }
-    }, []);
+    const handleNewComment = (newComment: CommentPopUserDTO) => {
+        setComments(prevState => {
+            return [newComment, ...prevState];
+        });
+    }
 
     return (
-        <div>
+        <CommentSectionContainer>
+            <h2>Comments</h2>
             {allowComments &&
-                <CommentForm slug={slug} onSubmit={handleNewComment} />
-            }
-            <CommentList
-                slug={slug}
-                comments={comments?.comments || []}
-                onDelete={handleDeleteComment}
-            />
-        </div>
-    );
-};
+                <CommentForm slug={slug} onSubmit={handleNewComment}/>}
+            {comments.map(comment => (
+                <div key={comment._id}>
+                    <p>{comment.latestContent?.comment}</p>
+                </div>
+            ))}
+        </CommentSectionContainer>
+    )
+}
 
-export default React.memo(CommentSection);
-
-// CONTINUE: need to be able to modify the comments tree directly, to delete reply. Currently if reply and then delete the reply nothing happens.
+export default CommentSection;
