c6ff4f072d491fdd8019a53b7fe8d99b5c1a0d0b
dereckmezquita <dereck@mezquita.io>
2023-10-11 19:54:24 -0500

    Organisation.

--- client/src/components/pages/post-page/mdxComponents.tsx
+++ client/src/components/pages/post-page/mdxComponents.tsx
@@ -1,11 +1,11 @@
 import Figure from '@components/pages/mdx-elements/Figure';
-import DropCap from '@components/ui/DropCap';
+// import DropCap from '@components/pages/index-page/DropCap';
 import Alert from '@components/pages/mdx-elements/Alert';
 import Blockquote from '@components/pages/mdx-elements/Blockquote';
 
 export const mdxComponents = {
     Figure,
-    DropCap,
+    // DropCap,
     Alert,
     Blockquote,
 };
