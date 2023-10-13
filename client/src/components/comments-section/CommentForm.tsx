6fc9ce426c5141ddccf58150fd6e6be96ae31ab2
dereckmezquita <dereck@mezquita.io>
2023-10-12 23:44:03 -0500

    Edit, delete, and reply all work now.

--- client/src/components/comments-section/CommentForm.tsx
+++ client/src/components/comments-section/CommentForm.tsx
@@ -7,6 +7,7 @@ import api_new_comment from '@utils/api/interact/new_comment';
 // --------------------------------------
 import Button from '@components/atomic/Button';
 import IndicateLoading from '@components/atomic/IndicateLoading';
+import api_edit_comment from '@utils/api/interact/edit_comment';
 
 const Form = styled.form`
     background-color: ${theme.container.background.colour.primary()};
@@ -31,15 +32,35 @@ interface CommentFormProps {
     slug: string;
     parentComment?: string;
     onSubmit: (comment: CommentPopUserDTO) => void; // callback sends new comment to parent component
+    editComment?: CommentPopUserDTO;
 }
 
-const CommentForm: React.FC<CommentFormProps> = ({ slug, parentComment, onSubmit }) => {
-    const [commentInput, setCommentInput] = useState('');
+const CommentForm: React.FC<CommentFormProps> = ({ slug, parentComment, onSubmit, editComment }) => {
+    const [commentInput, setCommentInput] = useState(editComment?.latestContent?.comment || '');
     const [isLoading, setIsLoading] = useState(false);
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setIsLoading(true);
+
+        if (editComment) {
+            // If defaultValue prop is passed, this form is being used to edit a comment
+            // so we need to call the edit comment API instead of new comment API
+            try {
+                const res: CommentPopUserDTO = await api_edit_comment(editComment._id!, commentInput);
+                setIsLoading(false);
+                if (res) {
+                    res.childComments = editComment.childComments;
+                    onSubmit(res);
+                }
+            } catch (error) {
+                setIsLoading(false);
+            }
+
+            return;
+        }
+
+
         if (commentInput.trim()) {
             try {
                 const res = await api_new_comment(commentInput, slug, parentComment);
