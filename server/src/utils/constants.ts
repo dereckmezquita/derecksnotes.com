6924e7102813e02b50ff8bd7f3d721852292abfc
dereckmezquita <dereck@mezquita.io>
2023-10-13 18:55:33 -0500

    Prod mode outputs.

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: path.join(__dirname, '..', '..', '..', '.env') });
 
 export function DATETIME_YYYY_MM_DD_HHMMSS(): string {
     return new Date().toISOString().replace('T', '-').slice(0, 19).replace(/:/g, '');
@@ -7,12 +9,9 @@ import path from 'path';
 
 export const ROOT_DIR_CLIENT: string = path.join(__dirname, '..', '..', '..', 'client');
 
-export const ROOT_DIR_CLIENT_UPLOADS: string = path.join(ROOT_DIR_CLIENT, 'public', 'site-images', 'uploads', 'profile-photos');
-
-import dotenv from 'dotenv';
-dotenv.config({ path: path.join(__dirname, '..', '..', '..', '.env') });
+export const ROOT_DIR_CLIENT_UPLOADS: string = path.join(ROOT_DIR_CLIENT, process.env.PROD_MODE ? 'out' : 'public', 'site-images', 'uploads', 'profile-photos');
 
 // if in prod nginx picks up requests to /api/v3 and forwards to server
-export const API_PREFIX: string = process.env.NODE_ENV === 'prod' ? '' : '/api/v3';
+export const API_PREFIX: string = process.env.PROD_MODE ? '' : '/api/v3';
 
 export const MAX_COMMENT_DEPTH: number = 5;
