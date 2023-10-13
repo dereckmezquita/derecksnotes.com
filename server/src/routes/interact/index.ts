012b4d9a1c6723dad00cb07346634485eae8e5e5
dereckmezquita <dereck@mezquita.io>
2023-10-12 23:45:06 -0500

    Edit comment end point.

--- server/src/routes/interact/index.ts
+++ server/src/routes/interact/index.ts
@@ -5,6 +5,7 @@ import get_article_comments from './get_article_comments';
 import get_user_public_info from './get_user_public_info';
 import get_comments_threads_by_id from './get_comments_threads_by_id';
 import delete_comments from './delete_comments';
+import edit_comment from './edit_comment';
 
 const interactRoutes = Router();
 
@@ -15,5 +16,6 @@ interactRoutes.use('/', get_comments_threads_by_id);
 // auth protected routes
 interactRoutes.use('/', new_comment);
 interactRoutes.use('/', delete_comments);
+interactRoutes.use('/', edit_comment);
 
 export default interactRoutes;
