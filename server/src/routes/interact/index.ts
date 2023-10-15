ab7047141215b7d421816f1d17a2d3e070d3265b
dereckmezquita <dereck@mezquita.io>
2023-10-15 16:11:27 -0500

    Add routes.

--- server/src/routes/interact/index.ts
+++ server/src/routes/interact/index.ts
@@ -6,6 +6,8 @@ import get_user_public_info from './get_user_public_info';
 import get_comments_threads_by_id from './get_comments_threads_by_id';
 import delete_comments from './delete_comments';
 import edit_comment from './edit_comment';
+import update_user_info from './update_user_info';
+import delete_geolocations from './delete_geolocation';
 
 const interactRoutes = Router();
 
@@ -17,5 +19,7 @@ interactRoutes.use('/', get_comments_threads_by_id);
 interactRoutes.use('/', new_comment);
 interactRoutes.use('/', delete_comments);
 interactRoutes.use('/', edit_comment);
+interactRoutes.use('/', update_user_info);
+interactRoutes.use('/', delete_geolocations);
 
 export default interactRoutes;
