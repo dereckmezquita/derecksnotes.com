4ffa32de80567a68b6e50f2dd9fe313177530289
dereckmezquita <dereck@mezquita.io>
2023-10-15 21:00:46 -0500

    Re-orgnaisation getting rid of old code.

--- test/geo-locate-ip-address.ts
+++
@@ -1,8 +0,0 @@
-
-import { locateIP } from '../server/src/modules/ip_locate';
-
-const ip_address: string = "75.80.232.56";
-
-locateIP(ip_address).then((res) => {
-    console.log(res);
-});
---
+++ writing/README.md
@@ -0,0 +1,3 @@
+# writing
+
+I'll keep drafts or things I am in the process of writing/content for the website.
