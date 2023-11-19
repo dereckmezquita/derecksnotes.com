16974f5f056d787d847324ea569ecf00a0313237
dereckmezquita <dereck@mezquita.io>
2023-11-19 10:58:53 -0600

    Better detect dev mode.

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,12 +11,12 @@ import { authRoutes, uploadRoutes, interactRoutes } from './routes/index';
 import { API_PREFIX } from '@utils/constants';
 import { DatabaseConnector, MongoDBConnector } from '@utils/DatabaseConnector';
 
-const dev_mode: boolean = process.env.NEXT_PUBLIC_DEV_MODE === 'true';
+const DEV_MODE: boolean = process.env.NEXT_PUBLIC_DEV_MODE === 'true';
 
 dotenv.config({ path: '../.env' });
-const PORT: number = dev_mode ? 3004 : 3003;
+const PORT: number = DEV_MODE ? 3004 : 3003;
 
-console.log(`Detected: ${dev_mode ? 'Development' : 'Production'} Mode\nUsing port: ${PORT}`);
+console.log(`Detected: ${DEV_MODE ? 'Development' : 'Production'} Mode\nUsing port: ${PORT}`);
 
 // ----------------------------------------
 // Setup Redis client and session store
@@ -32,7 +32,7 @@ const redisStore = makeRedisStore(session);
 // ----------------------------------------
 export const app = express();
 
-if (dev_mode) {
+if (DEV_MODE) {
     // assuming frontend on port 3000
     app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
     console.log('CORS enabled for localhost:3000');
@@ -54,8 +54,8 @@ export async function SetUp(dbConnector: DatabaseConnector): Promise<void> {
             resave: false, // forces session be saved back to the session store, even if the session was never modified during the request
             saveUninitialized: false,
             cookie: { // NOTE: if session data is not being saved in client check these
-                secure: dev_mode, // HTTPS in production
-                httpOnly: true, // true, // cookie inaccessible from JavaScript running in the browser
+                secure: !DEV_MODE, // HTTPS in production
+                httpOnly: !DEV_MODE, // true, // cookie inaccessible from JavaScript running in the browser
                 // days * hours * minutes * seconds * milliseconds
                 maxAge: 30 * 24 * 60 * 60 * 1000 // 1 day in milliseconds
             }
