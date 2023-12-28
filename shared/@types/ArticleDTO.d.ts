662bb35be36e1d007a98c18e285f005a7c79da5e
dereckmezquita <dereck@mezquita.io>
2023-12-28 12:03:17 -0600

    DTO for articles.

---
+++ shared/@types/ArticleDTO.d.ts
@@ -0,0 +1,16 @@
+export { };
+
+declare global {
+    interface ArticleDTO {
+        slug: string; // client should send filename of the article
+        judgement?: Map<string, 'like' | 'dislike'>; // not sent; save bandwidth
+
+        // Virtuals; optional because they are not always present
+        likesCount?: number;
+        dislikesCount?: number;
+        totalJudgement?: number;
+
+        // calculated by end point
+        likedByCurrentUser?: boolean;
+    }
+}
