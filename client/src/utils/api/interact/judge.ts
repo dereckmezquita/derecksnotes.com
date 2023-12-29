55f1b3e5a84435c47f400f1d27da22a8a763486f
dereckmezquita <dereck@mezquita.io>
2023-12-29 11:56:40 -0600

    Separate functions for comments and articles.c

--- client/src/utils/api/interact/judge.ts
+++ client/src/utils/api/interact/judge.ts
@@ -30,3 +30,53 @@ const api_judge = async (type: 'article' | 'comment', id: string, judgement: str
 }
 
 export default api_judge;
+
+export const api_judge_article = async (slug: string, judgement: 'like' | 'dislike') => {
+    try {
+        const response = await fetch(API_PREFIX + '/interact/judge_article', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                slug,
+                judgement
+            }),
+            credentials: 'include'
+        });
+
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error(data.message || 'Error occurred while judging.');
+        }
+
+        return data;
+    } catch (err: any) {
+        console.log(err);
+    }
+}
+
+export const api_judge_comment = async (id: string, judgement: 'like' | 'dislike') => {
+    try {
+        const response = await fetch(API_PREFIX + '/interact/judge_comment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id,
+                judgement
+            }),
+            credentials: 'include'
+        });
+
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error(data.message || 'Error occurred while judging.');
+        }
+
+        return data;
+    } catch (err: any) {
+        console.log(err);
+    }
+}
