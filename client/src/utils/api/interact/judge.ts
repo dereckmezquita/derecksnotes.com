0dae74076d68fc5194f85cb469b455a5899d8860
dereckmezquita <dereck@mezquita.io>
2024-04-25 17:29:22 -0500

    Separate judge functions.

--- client/src/utils/api/interact/judge.ts
+++ client/src/utils/api/interact/judge.ts
@@ -1,36 +1,5 @@
 import { API_PREFIX } from "@constants/config";
 
-const api_judge = async (type: 'article' | 'comment', id: string, judgement: string) => {
-    const END_POINT: string = type === 'article' ?
-        '/interact/judge_article' :
-        '/interact/judge_comment';
-
-    try {
-        const response = await fetch(API_PREFIX + END_POINT, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                id: id, // encodeURIComponent(id)
-                judgement
-            }),
-            credentials: 'include'
-        });
-
-        const data = await response.json();
-        if (!response.ok) {
-            throw new Error(data.message || 'Error occurred while judging.');
-        }
-
-        return data;
-    } catch (error) {
-        throw error;
-    }
-}
-
-export default api_judge;
-
 export const api_judge_article = async (slug: string, judgement: 'like' | 'dislike') => {
     try {
         const response = await fetch(API_PREFIX + '/interact/judge_article', {
