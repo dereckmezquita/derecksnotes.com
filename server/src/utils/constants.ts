11f8d979c71afb1a6a251fedbb168246727ace32
dereckmezquita <dereck@mezquita.io>
2024-06-16 17:51:57 -0500

    DN-2: Some constants.

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -1,3 +1,13 @@
 const SESSION_MAX_AGE_DAYS: number = 30;
 export const SESSION_MAX_AGE: number =
     SESSION_MAX_AGE_DAYS * 24 * 60 * 60 * 1000;
+
+export function DATETIME_YYYY_MM_DD_HHMMSS(): string {
+    return new Date()
+        .toISOString()
+        .replace('T', '-')
+        .slice(0, 19)
+        .replace(/:/g, '');
+}
+
+export const MAX_COMMENT_DEPTH: number = 5;
