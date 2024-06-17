db34ee6b4535f7733f4d9f90b135aca535168983
dereckmezquita <dereck@mezquita.io>
2024-06-17 09:41:00 -0500

    Application metadata and working on Index page which is used to display posts.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,6 +3,7 @@ import path from 'path';
 
 import { API_URL, APP_VERSION } from '@components/lib/env';
 import {
+    APPLICATION_DEFAULT_METADATA,
     APPLICATION_DESCRIPTION,
     ROOT_DIR_APP
 } from '@components/lib/constants';
@@ -12,6 +13,7 @@ import {
     PostMetadata,
     fetchPostsMetadata
 } from '@components/utils/mdx/fetchPostsMetadata';
+import { Index } from '@components/components/pages/index/Index';
 
 export const metadata: Metadata = {
     title: 'DN | Blog',
@@ -22,13 +24,7 @@ async function Page() {
     const posts: PostMetadata[] = fetchPostsMetadata(
         path.join(ROOT_DIR_APP, 'posts')
     );
-    return (
-        <div>
-            <h1>Next.js App</h1>
-            <h2>Version: {APP_VERSION}</h2>
-            <h2>API URL: {API_URL}</h2>
-        </div>
-    );
+    return <Index posts={posts} meta={APPLICATION_DEFAULT_METADATA} />;
 }
 
 export default Page;
