071d3b5fbb1b12da8ac187df791bedd0c891d066
dereckmezquita <dereck@mezquita.io>
2024-06-17 20:55:27 -0500

    Added mdx components.

--- client/src/utils/mdx/processMdx.ts
+++ client/src/utils/mdx/processMdx.ts
@@ -1,12 +1,11 @@
 import { theme } from '@components/styles/theme';
 
-import rehypeExternalLinks from 'rehype-external-links';
+import rehypeExternalLinks from 'rehype-external-links'; // adds target="_blank" to external links
 import rehypePrettyCode, { type Options } from 'rehype-pretty-code';
 import remarkGfm from 'remark-gfm'; // github flavoured md
 import remarkMath from 'remark-math'; // allows maths in mdx
 import remarkUnwrapImages from 'remark-unwrap-images'; // removes p tag around images
 import rehypeMathJax from 'rehype-mathjax'; // renders maths
-import remarkExternalLinks from 'remark-external-links'; // adds target="_blank" to external links
 import rehypeSlug from 'rehype-slug'; // adds id to headings
 
 // custom plugins
@@ -18,6 +17,8 @@ import rehypeDropCap from '../remark-rehype/rehypeDropCap';
 import { PostMetadata } from './fetchPostsMetadata';
 import { compileMDX } from 'next-mdx-remote/rsc';
 
+import mdxComponents from '@components/components/mdx/index';
+
 const rehypePrettyCodeOptions: Partial<Options> = {
     theme: 'github-dark-dimmed',
     defaultLang: 'plaintext'
@@ -26,6 +27,7 @@ const rehypePrettyCodeOptions: Partial<Options> = {
 export async function processMdx(markdown: string) {
     const { content, frontmatter } = await compileMDX<PostMetadata>({
         source: markdown,
+        components: mdxComponents,
         options: {
             parseFrontmatter: true,
             // REF: https://github.com/hashicorp/next-mdx-remote/issues/356#issuecomment-1556074660
