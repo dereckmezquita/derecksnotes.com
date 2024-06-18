48662814e70d25e5ee269681bafcf39d4d99cb1c
dereckmezquita <dereck@mezquita.io>
2024-06-18 17:40:53 -0500

    DN-2: Helper functions to get definitions; with dynamic types.

--- client/src/utils/dictionaries/fetchDefinitionMetadata.ts
+++ client/src/utils/dictionaries/fetchDefinitionMetadata.ts
@@ -1,6 +1,8 @@
+import fs from 'fs';
 import path from 'path';
 
 import { stripMdx } from '../mdx/fetchPostsMetadata';
+import { ROOT_DIR_APP } from '@components/lib/constants';
 
 export interface DefinitionMetadata {
     slug: string;
@@ -44,4 +46,48 @@ export function extractSingleDefinitionMetadata(
         console.error(error);
         process.exit(1);
     }
+}
+
+export function fetchDefinitionsMetadata(folder: string): DefinitionMetadata[] {
+    const files: string[] = fs.readdirSync(folder);
+    const mdx: string[] = files.filter((file) => file.endsWith('.mdx'));
+
+    let defs: DefinitionMetadata[] = mdx.map((file) => {
+        return extractSingleDefinitionMetadata(path.join(folder, file));
+    });
+
+    defs = defs.filter((def) => def.published);
+
+    // sort by letter; letters can be a-z and symbols/not letters should come last
+    // sort by letter; letters can be a-z and symbols/not letters should come last
+    return defs.sort((a, b) => {
+        const letterA = a.letter.toLowerCase();
+        const letterB = b.letter.toLowerCase();
+
+        const isLetterA = /^[a-z]$/.test(letterA);
+        const isLetterB = /^[a-z]$/.test(letterB);
+
+        if (isLetterA && isLetterB) {
+            return letterA.localeCompare(letterB);
+        } else if (isLetterA) {
+            return -1;
+        } else if (isLetterB) {
+            return 1;
+        } else {
+            return letterA.localeCompare(letterB);
+        }
+    });
+}
+
+export function getDefinitionsWithDictionary(
+    dictionary: string
+): DefinitionMetadata[] {
+    const defs: DefinitionMetadata[] = fetchDefinitionsMetadata(
+        path.join(ROOT_DIR_APP, 'dictionaries', dictionary, 'definitions')
+    );
+
+    return defs.map((def) => ({
+        ...def,
+        dictionary
+    }));
 }
