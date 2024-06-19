c4c1e47a035275660e1ff07d3bad025e4a48d750
dereckmezquita <dereck@mezquita.io>
2024-06-19 18:09:49 -0500

    DN-2: Fix import.

--- client/src/utils/mdx/processMdx.ts
+++ client/src/utils/mdx/processMdx.ts
@@ -15,7 +15,7 @@ import rehypeAddHeadingLinks from '../remark-rehype/rehypeAddHeadingLinks';
 import rehypeDropCap from '../remark-rehype/rehypeDropCap';
 
 import { PostMetadata } from './fetchPostsMetadata';
-import { DefinitionMetadata } from '@components/app/dictionaries/biology/page';
+import { DefinitionMetadata } from '../dictionaries/fetchDefinitionMetadata';
 import { compileMDX } from 'next-mdx-remote/rsc';
 
 import mdxComponents from '@components/components/mdx/index';
