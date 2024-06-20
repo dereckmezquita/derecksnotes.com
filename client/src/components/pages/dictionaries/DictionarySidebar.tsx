95849f3083f6a6ab832ef32d1559f15d18ce1d02
dereckmezquita <dereck@mezquita.io>
2024-06-19 19:08:42 -0500

    DN-2: Working on DictionarySidebar.

---
+++ client/src/components/pages/dictionaries/DictionarySidebar.tsx
@@ -0,0 +1,11 @@
+import { SideBarContainer, SideBarSiteName } from "../posts-dictionaries";
+
+function DictionarySidebar() {
+    return (
+        <SideBarContainer>
+            <SideBarSiteName fontSize="20px">{`Dereck's Notes`}</SideBarSiteName>
+        </SideBarContainer>
+    );
+}
+
+export default DictionarySidebar;
