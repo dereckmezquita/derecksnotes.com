e56b52767aaaa73331d93f22f475476c5ebc0d8c
dereckmezquita <dereck@mezquita.io>
2024-07-15 13:21:45 -0500

    DN-4: Fix spacing between containers.

--- client/src/app/test/api/modules/BoxContainer.tsx
+++ client/src/app/test/api/modules/BoxContainer.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Box = styled.div`
-    margin: 10px;
     padding: 20px;
     text-decoration: none;
     color: inherit;
-
-    padding: 5px;
-    text-decoration: none;
-
     border: 1px solid #ccc;
     border-radius: 5px;
     text-align: center;
@@ -19,10 +14,20 @@ const Box = styled.div`
     }
 `;
 
+const BoxWrapper = styled.div`
+    width: 50%;
+    margin: 0 auto 20px;
+    &:last-child {
+        margin-bottom: 0; // NOTE: removes margin from last child
+    }
+`;
+
 export function BoxContainer({ children }: { children: React.ReactNode }) {
     return (
-        <Box style={{ width: '50%', margin: 'auto' }}>
-            <div style={{ padding: '20px' }}>{children}</div>
-        </Box>
+        <BoxWrapper>
+            <Box>
+                <div style={{ padding: '20px' }}>{children}</div>
+            </Box>
+        </BoxWrapper>
     );
 }
