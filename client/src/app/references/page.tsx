c62f2a915a0b92aecd61bb764348de781358bb85
Dereck Mezquita <dereck@mezquita.io>
2024-12-24 12:50:19 -0600

    DN-6: Renamed function that gets the posts from a section; it now filters to only get the top level posts and exclude the series.

--- client/src/app/references/page.tsx
+++ client/src/app/references/page.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 import { APPLICATION_DESCRIPTION } from '@lib/constants';
 import { Metadata } from 'next';
 
-import {
-    PostMetadata,
-    getPostsWithSection
-} from '@utils/mdx/fetchPostsMetadata';
+import { PostMetadata, getSectionPosts } from '@utils/mdx/fetchPostsMetadata';
 import { Index } from '@components/pages/index/Index';
 
 export const metadata: Metadata = {
@@ -15,7 +12,7 @@ export const metadata: Metadata = {
 };
 
 async function Page() {
-    const posts: PostMetadata[] = getPostsWithSection('references');
+    const posts: PostMetadata[] = getSectionPosts('references');
 
     return <Index posts={posts} />;
 }
