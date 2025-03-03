7bb2b543afe25c72fc0e2c11b4704249d040fc0b
Dereck Mezquita <dereck@mezquita.io>
2025-03-03 14:32:13 -0600

    DN-8: Mount on API.

--- server/src/index.ts
+++ server/src/index.ts
@@ -64,10 +64,10 @@ app.get('/', async (req: Request, res: Response) => {
     res.json(status);
 });
 
-app.use('/', routes.auth);
-app.use('/', routes.comments);
-app.use('/', routes.profile);
-app.use('/', routes.test);
+app.use('/api/', routes.auth);
+app.use('/api/', routes.comments);
+app.use('/api/', routes.profile);
+app.use('/api/', routes.test);
 // -----
 
 // Add debugging middleware
